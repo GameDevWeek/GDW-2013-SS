b132af7d57e7a825811d70a72933f889ce24988c
lusito.info <lusito.info@googlemail.com>
2013-07-27 00:58:11 +0000

    Adding try-with-resources to map loader

--- src/de/fhtrier/gdw/commons/tiled/decoders/CsvDataDecoder.java
+++ src/de/fhtrier/gdw/commons/tiled/decoders/CsvDataDecoder.java
@@ -23,4 +23,8 @@ public class CsvDataDecoder implements IDataDecoder {
         }
         return Integer.parseInt(values[index++].trim());
     }
+
+    @Override
+    public void close() throws IOException {
+    }
 }
