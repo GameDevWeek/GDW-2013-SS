82facff2e08cae2de56c5646c375542bcad6d095
Janina.Noell@gmx.de <Janina.Noell@gmx.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-29 14:02:26 +0000

    AssetLoader kann Image und Sound laden, Animation folgt.

--- trunk/src/de/fhtrier/gdw/ss2013/util/AssetLoader.java
+++ trunk/src/de/fhtrier/gdw/ss2013/util/AssetLoader.java
@@ -9,36 +9,73 @@
 package de.fhtrier.gdw.ss2013.util;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
 import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Sound;
 
 import de.fhtrier.gdw.commons.jackson.JacksonReader;
 
 public class AssetLoader {
-
+    HashMap<String, Image> imageMap = new HashMap<>();
+    HashMap<String, Animation> animMap = new HashMap<>();
+    HashMap<String, Sound> soundMap = new HashMap<>();
+    
     public AssetLoader() {
-        // TODO Auto-generated constructor stub
+        setupImages("res/json/images.json");
+        setupAnimation("res/json/animation.json");
+        setupSound("res/json/sound.json");
     }
-
-    public static Image loadImage(String filename, String name){
-        List<ImageInfo> imageInfos;
-
-            try {
-                imageInfos = JacksonReader.readList(filename, ImageInfo.class);
-                for (ImageInfo imageInfo : imageInfos) {
-                    if (imageInfo.name.equals(name)) {
-                        return new Image(imageInfo.pfad);
-                    }
-                }
-               // return new Image(imageInfo.pfad);
-            } catch (Exception e) {
-                e.printStackTrace();
+    
+    public void setupImages(String filename) {
+        try {
+            List<ImageInfo> imageInfos = JacksonReader.readList(filename, ImageInfo.class);
+            for (ImageInfo imageInfo : imageInfos) {
+                imageMap.put(imageInfo.name, new Image(imageInfo.pfad));
             }
-        
-        return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+    
+    public void setupAnimation(String filename) {
+        try {
+            List<AnimInfo> animInfos = JacksonReader.readList(filename, AnimInfo.class);
+            for (AnimInfo animInfo : animInfos) {
+                //animMap.put(animInfo.name, new Animation());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void setupSound(String filename) {
+        try {
+            List<SoundInfo> soundInfos = JacksonReader.readList(filename, SoundInfo.class);
+            for (SoundInfo soundInfo : soundInfos) {
+                soundMap.put(soundInfo.name, new Sound(soundInfo.pfad));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public Image getImage(String name) {
+        return imageMap.get(name);
+    }
+    
+    public Animation getAnimation(String name) {
+        return animMap.get(name);
+    }
+    
+    public Sound getSound(String name) {
+        return soundMap.get(name);
+    }
+    
     public static void main(String args[]){
         System.setProperty("org.lwjgl.librarypath",
                 new File(
@@ -46,7 +83,9 @@ public class AssetLoader {
                         LWJGLUtil.getPlatformName()).getAbsolutePath());
         System.setProperty("net.java.games.input.librarypath",
                 System.getProperty("org.lwjgl.librarypath"));
-        System.out.println(loadImage("res/json/images.json", "testbild"));
+        
+        AssetLoader loader = new AssetLoader();
+        System.out.println(loader.getImage("testbild"));
         
     }
     
