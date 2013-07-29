6c8782ac9f45a21071f06aab46f171115d824f1f
jusweinberg@gmail.com <jusweinberg@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-29 12:40:06 +0000

    LevelTeam: Bugfix Konstruktor Astronaut und Alien

--- trunk/src/de/fhtrier/gdw/ss2013/game/Alien.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/Alien.java
@@ -1,12 +1,14 @@
 package de.fhtrier.gdw.ss2013.game;
 
+import org.newdawn.slick.geom.Vector2f;
+
 public class Alien extends Player{
 
     private float mana;
     private float maxMana;
     
     public Alien(float x, float y, float maxMana) {
-        super(x, y);
+        super(new Vector2f (x,y));
         this.maxMana= maxMana;
         setMana(maxMana);
         // TODO Auto-generated constructor stub
