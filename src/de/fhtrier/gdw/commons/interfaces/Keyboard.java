cdc7b7eb51283f802aab48d6fb96e4d8553b2ce9
travelvalic <travelvalic@gmail.com>
2013-07-29 11:23:52 +0000

    Team Input - Dennis, Valentin

---
+++ src/de/fhtrier/gdw/commons/interfaces/Keyboard.java
@@ -0,0 +1,9 @@
+package de.fhtrier.gdw.commons.interfaces;
+
+public interface Keyboard {
+    
+    public void keyReleased(int key, char c);
+ 
+    public void keyPressed(int key, char c);
+
+}
--- src/de/fhtrier/gdw/ss2013/game/Player.java
+++ src/de/fhtrier/gdw/ss2013/game/Player.java
@@ -3,12 +3,17 @@ package de.fhtrier.gdw.ss2013.game;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+
+import de.fhtrier.gdw.commons.interfaces.Keyboard;
+import de.fhtrier.gdw.commons.interfaces.Mouse;
+import de.fhtrier.gdw.ss2013.MainGame;
 
 /**
  * Player class
  */
-public class Player extends Entity {
+public class Player extends Entity implements Mouse, Keyboard {
 
     public Player(float x, float y) {
         super.position.set(x, y);
@@ -27,4 +32,52 @@ public class Player extends Entity {
     public void update(GameContainer container, int delta)
             throws SlickException {
     }
+    
+    
+    public void keyPressed(int key, char c)
+    {
+        if (key == Input.KEY_A) 
+        {
+           
+        }
+        else if (key == Input.KEY_D)
+        {
+            
+        }
+    }
+    
+    public void keyReleased(int key, char c)
+    {
+        if (key == Input.KEY_W || key == Input.KEY_SPACE)
+        {
+            
+        }
+    }
+    
+    
+    public void mouseMoved(int oldx, int oldy, int newx, int newy)
+    {
+        
+    }
+
+    public void mouseDragged(int oldx, int oldy, int newx, int newy)
+    {
+        
+    }
+
+    public void mouseReleased(int button, int x, int y)
+    {
+        
+    }
+
+    public void mousePressed(int button, int x, int y)
+    {
+        
+    }
+    
+    public void mouseWheelMoved(int newValue)
+    {
+        
+    }
+   
 }
