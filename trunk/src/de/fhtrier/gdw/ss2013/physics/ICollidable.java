8751322f50ce8002bf70f3e2e898163ba33808b2
ribnon@gmail.com <ribnon@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-29 14:24:41 +0000

    added Interface ICollidable

---
+++ trunk/src/de/fhtrier/gdw/ss2013/physics/ICollidable.java
@@ -0,0 +1,10 @@
+package de.fhtrier.gdw.ss2013.physics;
+
+import org.jbox2d.dynamics.Fixture;
+
+import de.fhtrier.gdw.ss2013.game.Entity;
+
+public interface ICollidable {
+	public void onCollision(Entity e);
+	public Fixture getFixture();
+}
