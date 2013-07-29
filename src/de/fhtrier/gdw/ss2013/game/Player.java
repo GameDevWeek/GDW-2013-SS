a1be01f00290a1c2b3798e48b7fb0d78971b3aeb
travelvalic <travelvalic@gmail.com>
2013-07-29 11:40:46 +0000

    Team Input - Dennis, Valentin

--- src/de/fhtrier/gdw/ss2013/game/Player.java
+++ src/de/fhtrier/gdw/ss2013/game/Player.java
@@ -6,14 +6,12 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
-import de.fhtrier.gdw.commons.interfaces.Keyboard;
-import de.fhtrier.gdw.commons.interfaces.Mouse;
 import de.fhtrier.gdw.ss2013.MainGame;
 
 /**
  * Player class
  */
-public class Player extends Entity implements Mouse, Keyboard {
+public class Player extends Entity {
 
     public Player(float x, float y) {
         super.position.set(x, y);
@@ -33,51 +31,5 @@ public class Player extends Entity implements Mouse, Keyboard {
             throws SlickException {
     }
     
-    
-    public void keyPressed(int key, char c)
-    {
-        if (key == Input.KEY_A) 
-        {
-           
-        }
-        else if (key == Input.KEY_D)
-        {
-            
-        }
-    }
-    
-    public void keyReleased(int key, char c)
-    {
-        if (key == Input.KEY_W || key == Input.KEY_SPACE)
-        {
-            
-        }
-    }
-    
-    
-    public void mouseMoved(int oldx, int oldy, int newx, int newy)
-    {
-        
-    }
-
-    public void mouseDragged(int oldx, int oldy, int newx, int newy)
-    {
-        
-    }
-
-    public void mouseReleased(int button, int x, int y)
-    {
-        
-    }
-
-    public void mousePressed(int button, int x, int y)
-    {
-        
-    }
-    
-    public void mouseWheelMoved(int newValue)
-    {
-        
-    }
    
 }
