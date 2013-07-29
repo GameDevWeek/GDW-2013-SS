097a46e5cfcb460bb723eb4fd957008193467844
jusweinberg <jusweinberg@gmail.com>
2013-07-29 10:06:18 +0000

    LVL Team: Klasse für Türen hinzugefügt

---
+++ src/de/fhtrier/gdw/ss2013/game/entities/Door.java
@@ -0,0 +1,32 @@
+package de.fhtrier.gdw.ss2013.game.entities;
+
+import org.newdawn.slick.geom.Vector2f;
+
+import de.fhtrier.gdw.ss2013.game.Entity;
+
+public class Door extends Entity {
+
+    private boolean open;
+
+    public Door( Vector2f position) {
+        super(position);
+        open = false;
+    }
+
+    public Door(Vector2f position, boolean open) {
+        super(position);
+        this.open = open ;
+
+    }
+
+    public void open() {
+        if (!open) {
+            open = true;
+        }
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+
+}
