002de076c6057879ea3c4027abf1d51d8778e0e6
jusweinberg@gmail.com <jusweinberg@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-29 12:36:52 +0000

    LevelTeam: Methode collected definiert

--- trunk/src/de/fhtrier/gdw/ss2013/game/entities/OxygenBubble.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/entities/OxygenBubble.java
@@ -10,23 +10,30 @@ package de.fhtrier.gdw.ss2013.game.entities;
 
 import org.newdawn.slick.geom.Vector2f;
 
+import de.fhtrier.gdw.ss2013.game.Astronaut;
 import de.fhtrier.gdw.ss2013.game.Entity;
 
 public class OxygenBubble extends Entity {
 
-    float oxygenLevel;
-    
+    private float oxygenLevel;
+
     // Standard-Konstruktor
     public OxygenBubble() {
 
     }
 
     public OxygenBubble(Vector2f position, float oxygenLevel) {
-        super(position);
+        super(position.copy());
         this.oxygenLevel = oxygenLevel;
     }
 
-    public void collected() {
+    public void collected(Astronaut astro) {
+        float currentOxygen = astro.getOxygen();
+        if (currentOxygen + oxygenLevel < astro.getMaxOxygen()) {
+            astro.setOxygen(currentOxygen + oxygenLevel);
+        } else {
+            astro.setOxygen(astro.getMaxOxygen());
+        }
 
     }
 
