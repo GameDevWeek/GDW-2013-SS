5f06866dc0c793e8b9ab376d1b4924dd23304dfb
georg.peter.schaefer <georg.peter.schaefer@googlemail.com>
2013-07-29 11:31:36 +0000

    Moving Platform class added

---
+++ src/de/fhtrier/gdw/ss2013/game/entities/MovingPlatform.java
@@ -0,0 +1,22 @@
+package de.fhtrier.gdw.ss2013.game.entities;
+
+import org.newdawn.slick.geom.Vector2f;
+
+import de.fhtrier.gdw.ss2013.game.Entity;
+/**
+ * Moving Platform class
+ * @author Kevin, Georg
+ *
+ */
+public class MovingPlatform extends Entity {
+    private Vector2f velocity;
+    
+    public MovingPlatform(Vector2f pos, Vector2f velo) {
+        super(pos);
+        velocity = velo;
+    }
+    public MovingPlatform() {
+        this(new Vector2f(), new Vector2f());
+    }
+    public void onCollide() {}
+}
