79a6956c10d4b14e6ede63c2485aab520a3915dd
travelvalic@gmail.com <travelvalic@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-29 12:45:38 +0000

    Team Input - Dennis, Valentin

---
+++ trunk/src/de/fhtrier/gdw/ss2013/interfaces/AlienControls.java
@@ -0,0 +1,26 @@
+package de.fhtrier.gdw.ss2013.interfaces;
+
+import org.newdawn.slick.Input;
+
+
+/*
+ * Team Input
+ * Dennis, Valentin
+ * 
+ */
+
+public interface AlienControls {
+    
+    public int shootButton = Input.MOUSE_LEFT_BUTTON;
+    public int rotateWheel = 0; ///???????????????????????????????
+    public int abilityButton = Input.MOUSE_RIGHT_BUTTON;
+    
+    
+    public void shoot(int button);
+    
+    public void rotateAbilities(int value);
+    
+    public void useAbility(int button);
+    
+
+}
