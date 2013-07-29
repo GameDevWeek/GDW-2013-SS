d4fcdf4b654bb11d24a7f708464c3df370c6651b
Davidwichter <davidwichter@yahoo.de>
2013-07-29 10:54:39 +0000

    Included HUD-Classes (UI-Team)

---
+++ src/de/fhtrier/gdw/ss2013/renderer/Annotation.java
@@ -0,0 +1,26 @@
+package de.fhtrier.gdw.ss2013.renderer;
+
+import org.newdawn.slick.Font;
+
+public class Annotation {
+    
+    private float xpos;
+    private float ypos;
+    
+    private Font notation;
+    
+    public Annotation(){
+        
+    }
+    
+    public void update(){
+        
+    }
+    
+    public void render(){
+        
+    }
+    
+    
+    
+}
