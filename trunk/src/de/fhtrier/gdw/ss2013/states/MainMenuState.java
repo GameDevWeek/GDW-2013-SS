629447512b55e3cd0d2c7e1ec322b66d9bdc8eeb
krumholt@gmail.com <krumholt@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-30 13:56:45 +0000

    mainmenustate aufegraeumt

--- trunk/src/de/fhtrier/gdw/ss2013/states/MainMenuState.java
+++ trunk/src/de/fhtrier/gdw/ss2013/states/MainMenuState.java
@@ -1,6 +1,5 @@
 package de.fhtrier.gdw.ss2013.states;
 
-import org.newdawn.slick.AngelCodeFont;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -17,9 +16,6 @@ import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
  */
 public class MainMenuState extends BasicGameState {
 	private Font font;
-
-	public MainMenuState() {
-	}
 
 	@Override
 	public void init(final GameContainer container, final StateBasedGame game)
@@ -48,43 +44,10 @@ public class MainMenuState extends BasicGameState {
 	}
 
 	@Override
-	public void enter(GameContainer container, StateBasedGame game)
-			throws SlickException {
-	}
-
-	@Override
-	public void leave(GameContainer container, StateBasedGame game)
-			throws SlickException {
-	}
-
-	@Override
 	public void keyReleased(int key, char c) {
 		if (key == Input.KEY_ENTER) {
 			MainGame.changeState(MainGame.GAMEPLAY);
 		}
 	}
 
-	@Override
-	public void keyPressed(int key, char c) {
-	}
-
-	@Override
-	public void mouseMoved(int oldx, int oldy, int newx, int newy) {
-	}
-
-	@Override
-	public void mouseDragged(int oldx, int oldy, int newx, int newy) {
-	}
-
-	@Override
-	public void mouseReleased(int button, int x, int y) {
-	}
-
-	@Override
-	public void mousePressed(int button, int x, int y) {
-	}
-
-	@Override
-	public void mouseWheelMoved(int newValue) {
-	}
 }
