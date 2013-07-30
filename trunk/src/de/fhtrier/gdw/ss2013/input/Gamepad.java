dd54dbbe60eb8eb73963a17051a19aff5d747dc5
travelvalic@gmail.com <travelvalic@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-30 08:32:22 +0000

    Team Input - Dennis, Valentin Deviceabfrage hinzugefügt (Getter)

---
+++ trunk/src/de/fhtrier/gdw/ss2013/input/Gamepad.java
@@ -0,0 +1,18 @@
+package de.fhtrier.gdw.ss2013.input;
+
+import org.newdawn.slick.GameContainer;
+
+public class Gamepad extends InputDevice {
+
+    public Gamepad(GameContainer gc) {
+        super(gc);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ trunk/src/de/fhtrier/gdw/ss2013/input/Mouse.java
@@ -0,0 +1,18 @@
+package de.fhtrier.gdw.ss2013.input;
+
+import org.newdawn.slick.GameContainer;
+
+public class Mouse extends InputDevice {
+
+    public Mouse(GameContainer gc) {
+        super(gc);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
