445278a5825e412a838f98c86a7ebf8bc2ed7659
jusweinberg <jusweinberg@gmail.com>
2013-07-30 15:39:33 +0000

    LevelTeam: onColissions & images hinzugefügt

--- src/de/fhtrier/gdw/ss2013/game/world/objects/OxygenBubble.java
+++ src/de/fhtrier/gdw/ss2013/game/world/objects/OxygenBubble.java
@@ -16,60 +16,90 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Graphics;
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.Entity;
+import de.fhtrier.gdw.ss2013.game.EntityManager;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
 import de.fhtrier.gdw.ss2013.physics.ICollidable;
 
 
-public class OxygenBubble extends Entity implements ICollidable{
+public class OxygenBubble extends Entity implements ICollidable {
 
-	private float oxygenLevel;
-	private AssetLoader a = AssetLoader.getInstance();
-	private Image img = a.getImage("Sauerstoff");
-	// Standard-Konstruktor
-	public OxygenBubble(Vector2f position) {
-		super(position.copy());
-		a.getImage("Sauerstoff");
-		// Default
-		oxygenLevel = 0;
-	}
+    private float oxygenLevel;
+    private AssetLoader a = AssetLoader.getInstance();
+    private Image img = a.getImage("bubble");
+    private Vector2f velocity = new Vector2f(10, 10);
+    private EntityManager man;
 
-	public OxygenBubble(Vector2f position, float oxygenLevel) {
-		super(position.copy());
-		this.oxygenLevel = oxygenLevel;
-	}
-	
-	@Override
-    public void onCollision(Entity e)
-    {
-        if(e instanceof Astronaut)
-        {
-            if (((Astronaut)e).getOxygen() + oxygenLevel < ((Astronaut)e).getMaxOxygen())
-            {
-                ((Astronaut) e).setOxygen(((Astronaut)e).getOxygen()+oxygenLevel);
+    // Standard-Konstruktor
+    public OxygenBubble(Vector2f position) {
+        super(position.copy());
+        a.getImage("bubble");
+        // Default
+        oxygenLevel = 0;
+    }
+
+    public OxygenBubble(Vector2f position, float oxygenLevel) {
+        super(position.copy());
+        this.oxygenLevel = oxygenLevel;
+    }
+
+    @Override
+    public void onCollision(Entity e) {
+        if (e instanceof Astronaut) {
+            if (((Astronaut) e).getOxygen() + oxygenLevel < ((Astronaut) e)
+                    .getMaxOxygen()) {
+                ((Astronaut) e).setOxygen(((Astronaut) e).getOxygen()
+                        + oxygenLevel);
+            } else {
+                ((Astronaut) e).setOxygen(((Astronaut) e).getMaxOxygen());
             }
-            else{
-                ((Astronaut)e).setOxygen(((Astronaut)e).getMaxOxygen());
+        }//Collision von bubbles
+        if(e instanceof OxygenBubble) {
+            if(e.getPosition().x <= this.position.x)
+            {
+                this.position.x -= this.velocity.x;
+            }
+            if(e.getPosition().y <= this.position.y)
+            {
+                this.position.y -= this.velocity.y;
             }
         }
     }
-	
-	public float getOxygenLevel() {
-		return oxygenLevel;
-	}
 
-	public void setOxygenLevel(float oxygenLevel) {
-		this.oxygenLevel = oxygenLevel;
-	}
+    public float getOxygenLevel() {
+        return oxygenLevel;
+    }
 
-	public void render(GameContainer container, Graphics g)
-	        throws SlickException{
-	        img.draw(this.getPosition().x, this.getPosition().y);
-}
-	
-	@Override
-	public Fixture getFixture() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void update(GameContainer container, int delta) {
+        hover();
+    }
+
+    public void hover() {
+    
+        if(this.position.x >= this.velocity.x || this.position.y > this.velocity.y)
+        {
+            this.position.x -= this.velocity.x;
+            this.position.y -= this.velocity.y;
+        }
+        else if(this.position.x < this.velocity.x || this.position.y < this.velocity.y){
+            this.position.x += this.velocity.x;
+            this.position.y += this.velocity.y;
+        }
+    }
+
+    public void setOxygenLevel(float oxygenLevel) {
+        this.oxygenLevel = oxygenLevel;
+    }
+
+    public void render(GameContainer container, Graphics g)
+            throws SlickException {
+        img.draw(this.getPosition().x, this.getPosition().y);
+    }
+
+    @Override
+    public Fixture getFixture() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
