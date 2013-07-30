ecb67f7e103b983f82aff328c4c23c721eb39c0a
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-30 17:58:32 +0000

    Code strukturiert

--- trunk/src/de/fhtrier/gdw/ss2013/gui/utils/CenteredText.java
+++ trunk/src/de/fhtrier/gdw/ss2013/gui/utils/CenteredText.java
@@ -1,16 +1,11 @@
 package de.fhtrier.gdw.ss2013.gui.utils;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
-import org.newdawn.slick.Graphics;
 
 public class CenteredText {
-   
-    public static void draw(float x, float y,String string, Font font)
-    {
-        
-        
-        font.drawString(x-font.getWidth(string)/2,y-font.getHeight(string)/2,string);
+
+    public static void draw(float x, float y, String string, Font font) {
+        font.drawString(x - font.getWidth(string) / 2, y - font.getHeight(string) / 2, string);
     }
-    
+
 }
