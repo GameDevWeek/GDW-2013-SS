2def7ffc1115f2ed8069379e4f584b78446ff740
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-30 17:47:22 +0000

    Code strukturiert

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/AbstractEnemy.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/AbstractEnemy.java
@@ -21,7 +21,7 @@ public abstract class AbstractEnemy extends Entity {
     private float damage;
     private Animation current_ani;
     private String left_animation = "animtest", right_animation = "animtest",
-            current = "animtest", oben = "animtest", unten = "animtest";
+            current = "animtest";
 
     private AssetLoader asset = AssetLoader.getInstance();
 
@@ -60,31 +60,19 @@ public abstract class AbstractEnemy extends Entity {
     @Override
     public void render(GameContainer container, Graphics g)
             throws SlickException {
-        // TODO Auto-generated method stub
-        // links-rechts
-        // rechts-links
-
+        // Move right
         if (velocity.x > 0
                 && current_ani.equals(asset.getAnimation(right_animation))) {
             current_ani = asset.getAnimation(right_animation);
 
         }
+        // Move left
         if (velocity.x < 0
                 && current_ani.equals(asset.getAnimation(left_animation))) {
             current_ani = asset.getAnimation(left_animation);
-
-            // oben-unten
         }
-        /*if (velocity.x > 0) {
-            current_ani = asset.getAnimation(right_animation);
-
-        }
-        if (velocity.x < 0) {
-            current_ani = asset.getAnimation(left_animation);
-
-        }*/
+        
         current_ani.draw(this.getPosition().x - (current_ani.getWidth()/2), this.getPosition().y - (current_ani.getHeight()/2));
-
     }
 
     public void setLeft_animation(String left_animation) {
@@ -110,13 +98,4 @@ public abstract class AbstractEnemy extends Entity {
     public String getRight_animation() {
         return right_animation;
     }
-
-    public void setOben(String oben) {
-        this.oben = oben;
-    }
-
-    public void setUnten(String unten) {
-        this.unten = unten;
-    }
-
 }
