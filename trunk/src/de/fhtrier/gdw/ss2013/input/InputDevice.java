0ee9f8ec3287d25692e0c9e36dfe3ca565f5582e
hottehead@googlemail.com <hottehead@googlemail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-30 11:09:20 +0000

    Inputklassen refactored

--- trunk/src/de/fhtrier/gdw/ss2013/input/InputDevice.java
+++ trunk/src/de/fhtrier/gdw/ss2013/input/InputDevice.java
@@ -1,3 +1,4 @@
+
 package de.fhtrier.gdw.ss2013.input;
 
 import java.util.HashMap;
@@ -17,54 +18,47 @@ public abstract class InputDevice {
 	protected AstronautController astronautController = null;
 
 	// enum für actions
-	enum ACTIONS {
+	enum ACTION {
 		MOVEFORWARD, MOVEBACKWARD, JUMP, ACTION, SHOOT, TARGET, ROTATEABILITY, USEABILITY
 	};
 
 	// dynamsiche tastenzuweisungen
-	 HashMap<Integer, ACTIONS> keymapping;
-	 
+	HashMap<Integer, ACTION> keymapping;
 
-	public InputDevice(GameContainer gc) {
+	public InputDevice (GameContainer gc) {
 		container = gc;
 		keymapping = new HashMap<>();
-		
+		loadKeymapping();
 	}
 
-	public abstract void update();
-	
-    public abstract void loadKeymapping();
+	public abstract void update ();
 
-    /**
-     * TODO
-     * @param action
-     * @param key
-     */
-    public void setKey(ACTIONS action, int key){
-        
-    }
-    
-    /**
-     * TODO
-     */
-    public void saveKeymapping(){
-        
-    }
-    
-	public void setAlienController(AlienController ac) {
+	public abstract void loadKeymapping ();
+
+	/** TODO */
+	public void saveKeymapping () {
+
+	}
+
+	/** TODO
+	 * @param action
+	 * @param key */
+	public void setKey (ACTION action, int key) {
+
+	}
+
+	public int getKey (ACTION action) {
+		return 0;
+	}
+
+	public void setAlienController (AlienController ac) {
 		alienController = ac;
 	}
 
-	public void setAstronautController(AstronautController ac) {
+	public void setAstronautController (AstronautController ac) {
 		astronautController = ac;
 	}
 
-	/**
-	 * TODO
-	 * - Keymapping für alle Actions je nach InputDevice aus Config datei lesen und speichern // Actions zu den Keys
-	 * - Keymapping dynamisch ändern können 
-	 * - Getter für Keymapping
-	 * 
-	 * 
-	 */
+	/** TODO - Keymapping für alle Actions je nach InputDevice aus Config datei lesen und speichern // Actions zu den Keys -
+	 * Keymapping dynamisch ändern können - Getter für Keymapping */
 }
--- trunk/src/de/fhtrier/gdw/ss2013/input/Keyboard.java
+++ trunk/src/de/fhtrier/gdw/ss2013/input/Keyboard.java
@@ -15,38 +15,24 @@ public class Keyboard extends InputDevice {
 
 	public Keyboard(GameContainer gc) {
 		super(gc);
-		loadKeymapping();
 	}
 
 	@Override
 	public void update() {
 
-		HashSet<ACTIONS> actions = new HashSet<>(keymapping.values());
+		HashSet<ACTION> actions = new HashSet<>(keymapping.values());
 		for (int key : keymapping.keySet()) {
 			if (container.getInput().isKeyDown(key)) {
-				ACTIONS action = keymapping.get(key);
+				ACTION action = keymapping.get(key);
 				if (actions.contains(action)) {
 					actions.remove(action);
 					doAction(action);
 				}
 			}
 		}
-
-		// else if (container.getInput().isKeyDown(Input.KEY_A)) {
-		// astronautController.moveBackward();
-		// }
-		//
-		// else if (container.getInput().isKeyPressed(Input.KEY_SPACE)) {
-		// astronautController.jump();
-		// }
-		//
-		// else if (container.getInput().isKeyPressed(Input.KEY_E)) {
-		// astronautController.action();
-		// }
-
 	}
 
-	private void doAction(ACTIONS action) {
+	private void doAction(ACTION action) {
 		switch (action) {
 		case MOVEFORWARD:
 			astronautController.moveForward();
@@ -67,17 +53,17 @@ public class Keyboard extends InputDevice {
 
 	@Override
 	public void loadKeymapping() {
-		keymapping.put(Input.KEY_D, ACTIONS.MOVEFORWARD);
-		keymapping.put(Input.KEY_RIGHT, ACTIONS.MOVEFORWARD);
+		keymapping.put(Input.KEY_D, ACTION.MOVEFORWARD);
+		keymapping.put(Input.KEY_RIGHT, ACTION.MOVEFORWARD);
 
-		keymapping.put(Input.KEY_A, ACTIONS.MOVEBACKWARD);
-		keymapping.put(Input.KEY_LEFT, ACTIONS.MOVEBACKWARD);
+		keymapping.put(Input.KEY_A, ACTION.MOVEBACKWARD);
+		keymapping.put(Input.KEY_LEFT, ACTION.MOVEBACKWARD);
 
-		keymapping.put(Input.KEY_W, ACTIONS.JUMP);
-		keymapping.put(Input.KEY_UP, ACTIONS.JUMP);
+		keymapping.put(Input.KEY_W, ACTION.JUMP);
+		keymapping.put(Input.KEY_UP, ACTION.JUMP);
 
-		keymapping.put(Input.KEY_S, ACTIONS.ACTION);
-		keymapping.put(Input.KEY_DOWN, ACTIONS.ACTION);
+		keymapping.put(Input.KEY_S, ACTION.ACTION);
+		keymapping.put(Input.KEY_DOWN, ACTION.ACTION);
 
 	}
 
