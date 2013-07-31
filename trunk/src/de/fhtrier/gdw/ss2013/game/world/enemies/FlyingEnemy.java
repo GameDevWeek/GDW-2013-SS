9e5f8dd49c47a3a6e8ac0c4222e6de56266b3a06
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-31 08:47:31 +0000

    Entity beschwert sich, wenn im Konstruktor die Velocity aufgerufen wird (Exception) LevelLoader gibt ein bisschen besseren Output FlyingEnemy ein bisschen an alle Änderungen angepasst

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/FlyingEnemy.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/FlyingEnemy.java
@@ -10,6 +10,7 @@ import de.fhtrier.gdw.ss2013.game.Entity;
 import de.fhtrier.gdw.ss2013.game.EntityManager;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
 import de.fhtrier.gdw.ss2013.game.player.Player;
+import de.fhtrier.gdw.ss2013.game.world.World;
 import de.fhtrier.gdw.ss2013.physics.ICollidable;
 
 /**
@@ -20,119 +21,130 @@ import de.fhtrier.gdw.ss2013.physics.ICollidable;
  */
 public class FlyingEnemy extends AbstractEnemy implements ICollidable {
 
-	private float health, flytime, bolttime;
-	private float flyintelligence, boltintelligence;
-	private float factor;
-	private EntityManager m;
-	private Player p;
-	final static float DEBUG_ENTITY_HALFEXTEND = 5;
-	private String rechts="animtest", links="animtest", current="animtest";
+    private float health, flytime, bolttime;
+    private float flyintelligence, boltintelligence;
+    private float factor;
+    private EntityManager m;
+    private Player p;
+    final static float DEBUG_ENTITY_HALFEXTEND = 5;
+    private String rechts = "animtest", links = "animtest", current = "animtest";
 
-	public FlyingEnemy() {
-		flyintelligence = boltintelligence = (float)Math.random();
-	    if(flyintelligence > 0.5f) {
-	        this.getVelocity().y = 0.0f;
-	    } else {
-	        this.getVelocity().x = 0.0f;
-	    }
-	    if (boltintelligence >= 0.67) {
-	        factor = 100;
-	    } else if (boltintelligence <= 0.33) {
-	        factor = 0;
-	    } else {
-	        factor = 50;
-	    }
-	    setLeft_animation(links);
-	    setRight_animation(rechts);
-	    setCurrent(current);
-	}
-    
-    public void setHealth(float hp) {
-		health = hp;
+    public FlyingEnemy() {
+        this.health = 100f; // FIXME: dummy value!
+        this.m = World.getInstance().getEntityManager();
+        // FIXME: Player will never be set!
+        flyintelligence = boltintelligence = (float) Math.random();
+        // Don't use Velocity in the constructor!
+        // if(flyintelligence > 0.5f) {
+        // this.getVelocity().y = 0.0f;
+        // } else {
+        // this.getVelocity().x = 0.0f;
+        // }
+        if (boltintelligence >= 0.67) {
+            factor = 100;
+        }
+        else if (boltintelligence <= 0.33) {
+            factor = 0;
+        }
+        else {
+            factor = 50;
+        }
+        setLeft_animation(links);
+        setRight_animation(rechts);
+        setCurrent(current);
     }
 
-	@Override
-	public void onCollision(Entity e) {
-		if (e instanceof Astronaut) {
-			((Astronaut) e).setOxygen(((Astronaut) e).getOxygen()
-					- this.getDamage());
-		}
-	}
+    public void setHealth(float hp) {
+        health = hp;
+    }
 
-	public void reduceHealth(float dmg) {
-		health -= dmg;
-	}
+    @Override
+    public void onCollision(Entity e) {
+        if (e instanceof Astronaut) {
+            ((Astronaut) e).setOxygen(((Astronaut) e).getOxygen() - this.getDamage());
+        }
+    }
 
-	public void shoot(Player player, EntityManager m) {
-//		EnemyBullet b = (EnemyBullet) m.createEntity(EnemyBullet.class);
-//        childPhysics = this.getPosition();
-//        b.setPhysicsObject(childPhysics);
-//		b.getVelocity().x = 5 * calcPlayerDirection(player).x;
-//		b.getVelocity().y = 5 * calcPlayerDirection(player).y;
-//		b.setDamage(this.getDamage());
-//		b.setReferences(m);
-	}
+    public void reduceHealth(float dmg) {
+        health -= dmg;
+    }
 
-	public void render(GameContainer container, Graphics g)
-			throws SlickException {
-		//g.setColor(Color.blue);
-		//g.drawRect(position.x - DEBUG_ENTITY_HALFEXTEND, position.y
-			//	- DEBUG_ENTITY_HALFEXTEND, DEBUG_ENTITY_HALFEXTEND * 2,
-			//	DEBUG_ENTITY_HALFEXTEND * 2);
-	    super.render(container, g);
-	    
-		// g.drawString(this.hashCode(), position.x, position.y);
-	}
+    public void shoot(Player player, EntityManager m) {
+        // EnemyBullet b = (EnemyBullet) m.createEntity(EnemyBullet.class);
+        // childPhysics = this.getPosition();
+        // b.setPhysicsObject(childPhysics);
+        // b.getVelocity().x = 5 * calcPlayerDirection(player).x;
+        // b.getVelocity().y = 5 * calcPlayerDirection(player).y;
+        // b.setDamage(this.getDamage());
+        // b.setReferences(m);
+    }
 
-	public void update(GameContainer container, int delta)
-			throws SlickException {
-		// float dt = delta / 1000.f;
-		flytime += delta;
-		bolttime += delta;
-		// TODO clamp dt if dt > 1/60.f ?
-		this.getPosition().x += this.getVelocity().x;
-		this.getPosition().y += this.getVelocity().y;
-		if (flytime >= 3000) {
-			this.getVelocity().x = -this.getVelocity().x;
-			this.getVelocity().y = -this.getVelocity().y;
-			flytime = flytime % 3000;
-		}
-	    if (bolttime >= 1000 && calcPlayerDistance(p) < 500) {
-	        this.shoot(p, m);
-	        bolttime = bolttime % 1000;
-	    }
-	    if (health <= 0) {
-	        m.removeEntity(this);
-	    }
-	}
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        // g.setColor(Color.blue);
+        // g.drawRect(position.x - DEBUG_ENTITY_HALFEXTEND, position.y
+        // - DEBUG_ENTITY_HALFEXTEND, DEBUG_ENTITY_HALFEXTEND * 2,
+        // DEBUG_ENTITY_HALFEXTEND * 2);
+        super.render(container, g);
 
-	private Vector2f calcPlayerDirection(Player player) {
-		Vector2f direction = new Vector2f();
-		direction = calcPlayerPosition(player);
-		direction.normalise();
-		return direction;
-	}
-	private float calcPlayerDistance(Player player) {
-	    Vector2f direction = new Vector2f();
-	    direction = calcPlayerPosition(player);
-	    return (float) Math.sqrt((direction.x*direction.x)+(direction.y*direction.y));
-	}
-	private Vector2f calcPlayerPosition(Player player) {
+        // g.drawString(this.hashCode(), position.x, position.y);
+    }
+
+    public void update(GameContainer container, int delta) throws SlickException {
+        // float dt = delta / 1000.f;
+        flytime += delta;
+        bolttime += delta;
+        // TODO clamp dt if dt > 1/60.f ?
+        this.getPosition().x += this.getVelocity().x;
+        this.getPosition().y += this.getVelocity().y;
+        if (flytime >= 3000) {
+            this.getVelocity().x = -this.getVelocity().x;
+            this.getVelocity().y = -this.getVelocity().y;
+            flytime = flytime % 3000;
+        }
+        if (bolttime >= 1000 && calcPlayerDistance(p) < 500) {
+            this.shoot(p, m);
+            bolttime = bolttime % 1000;
+        }
+        if (health <= 0) {
+            m.removeEntity(this);
+        }
+    }
+
+    private Vector2f calcPlayerDirection(Player player) {
+        Vector2f direction = new Vector2f();
+        direction = calcPlayerPosition(player);
+        direction.normalise();
+        return direction;
+    }
+
+    private float calcPlayerDistance(Player player) {
+        Vector2f direction = new Vector2f();
+        direction = calcPlayerPosition(player);
+        if (direction == null) {
+            return Float.MAX_VALUE;
+        }
+        return (float) Math.sqrt((direction.x * direction.x) + (direction.y * direction.y));
+    }
+
+    private Vector2f calcPlayerPosition(Player player) {
         Vector2f direction = new Vector2f();
         Vector2f position = getPosition();
-        direction.x = player.getPosition().x - position.x+((float)Math.random()*factor);
-        direction.y = player.getPosition().y - position.y;
-        return direction;
-	}
+        if (player != null && player.getPosition() != null) {
+            direction.x = player.getPosition().x - position.x + ((float) Math.random() * factor);
+            direction.y = player.getPosition().y - position.y;
+            return direction;
+        }
+        return null;
+    }
 
-	@Override
-	public Fixture getFixture() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public void setReferences(EntityManager m, Player p) {
-	    this.m = m;
-	    this.p = p;
-	}
+    @Override
+    public Fixture getFixture() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setReferences(EntityManager m, Player p) {
+        this.m = m;
+        this.p = p;
+    }
 }
