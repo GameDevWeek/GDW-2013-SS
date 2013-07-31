5c142c12ab29910d1a6d76379c4415005082ef1e
georg.peter.schaefer <georg.peter.schaefer@googlemail.com>
2013-07-31 08:42:51 +0000

    Changed EntityCollidable to abstract

--- src/de/fhtrier/gdw/ss2013/game/EntityCollidable.java
+++ src/de/fhtrier/gdw/ss2013/game/EntityCollidable.java
@@ -3,18 +3,12 @@ package de.fhtrier.gdw.ss2013.game;
 import de.fhtrier.gdw.ss2013.physix.ICollisionListener;
 import de.fhtrier.gdw.ss2013.physix.PhysixObject;
 
-public class EntityCollidable extends Entity implements ICollisionListener {
+public abstract class EntityCollidable extends Entity implements ICollisionListener {
 
     @Override
-    public void beginContact(PhysixObject object) {
-        // TODO Auto-generated method stub
-        
-    }
+    public abstract void beginContact(PhysixObject object);
 
     @Override
-    public void endContact(PhysixObject object) {
-        // TODO Auto-generated method stub
-        
-    }
+    public abstract void endContact(PhysixObject object);
 
 }
