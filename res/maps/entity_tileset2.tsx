ecd3d72f0b45b88cb730ea871919dfa17f0bc842
Janina.Noell <Janina.Noell@gmx.de>
2013-07-31 17:18:23 +0000

    testmap als standard map

---
+++ res/maps/entity_tileset2.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="entity_tileset" tilewidth="64" tileheight="64">
+ <image source="entity_tileset.png" width="192" height="384"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="flyingenemy"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="groundenemy"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="door"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" value="switch"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" value="box"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="type" value="meteroid"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="type" value="oxygenflower"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="type" value="button"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="type" value="smallflyingenemy"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="type" value="middleflyingenemy"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="type" value="bigflyingenemy"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="type" value="smallgroundenemy"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="type" value="middlegroundenemy"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="type" value="biggroundenemy"/>
+  </properties>
+ </tile>
+</tileset>
