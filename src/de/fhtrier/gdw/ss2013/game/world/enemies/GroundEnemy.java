6dfe60dcd2bd067f0b9d24ddc38ab2105915b301
kkev5621 <kkev5621@gmail.com>
2013-07-31 11:53:15 +0000

    Stupid AI for Ground Enemy reworked

--- src/de/fhtrier/gdw/ss2013/game/world/enemies/GroundEnemy.java
+++ src/de/fhtrier/gdw/ss2013/game/world/enemies/GroundEnemy.java
@@ -7,8 +7,10 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
+import de.fhtrier.gdw.ss2013.game.EntityManager;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
 import de.fhtrier.gdw.ss2013.game.player.Player;
+import de.fhtrier.gdw.ss2013.game.world.World;
 import de.fhtrier.gdw.ss2013.physix.PhysixObject;
 
 /**
@@ -23,125 +25,157 @@ public class GroundEnemy extends AbstractEnemy {
     private Astronaut p;
     private Image img;
     private AssetLoader a = AssetLoader.getInstance();
-    private Vector2f lastvelocity, lastposition;
+    private EntityManager m;
+    private World w = World.getInstance();
+    private Vector2f lastposition;
     private float speed = 80;
 
-    private String rechts="animtest", links="animtest", current="animtest";
-    
-    
-	public GroundEnemy() {
-		img = a.getImage("groundEnemy");
-		if (Math.random() >= 0.5) {
-		    intelligence = true;
-		} else {
-		    intelligence = false;
-		}
-		normalMode = true;
-		huntMode = waitMode = false;
-		hunttime = 0;
-		moveLR = false;
-	    setLeft_animation(links);
-	    setRight_animation(rechts);
-	    setCurrent(current);
-	}
+    private String rechts = "animtest", links = "animtest",
+            current = "animtest";
 
-	public void render(GameContainer container, Graphics g)
-	         throws SlickException {
+    public GroundEnemy() {
+        img = a.getImage("groundEnemy");
+        /*
+         * if (Math.random() >= 0.5) { intelligence = true; } else {
+         * intelligence = false; }
+         */
+        intelligence = false;
+        normalMode = true;
+        huntMode = waitMode = false;
+        hunttime = 0;
+        moveLR = false;
+        setLeft_animation(links);
+        setRight_animation(rechts);
+        setCurrent(current);
+        p = w.getAstronaut();
+        m = w.getEntityManager();
+    }
+
+    public void render(GameContainer container, Graphics g)
+            throws SlickException {
         Vector2f position = getPosition();
-	    g.drawImage(img, position.x-(img.getWidth()/2), position.y-(img.getHeight()/2));
-	    // g.drawString(this.hashCode(), position.x, position.y);
-	}
+        g.drawImage(img, position.x - (img.getWidth() / 2),
+                position.y - (img.getHeight() / 2));
+        // g.drawString(this.hashCode(), position.x, position.y);
+    }
 
     public void update(GameContainer container, int delta)
             throws SlickException {
-        // float dt = delta / 1000.f;
-        movetime += delta;
-        // TODO clamp dt if dt > 1/60.f ?
-//        setVelocityX(speed);
-//        if (!intelligence) {
-//            if (movetime >= 3000) {
-//                this.getVelocity().x = -this.getVelocity().x;
-//                movetime = movetime % 3000;
-//            }
-//        } else {
-//            if(normalMode) {
-//                if (calcPlayerDistance(p) < 300) {
-//                    normalMode = false;
-//                    huntMode = true;
-//                } else {
-//                    if (movetime >= 3000) {
-//                        this.getVelocity().x = -this.getVelocity().x;
-//                        movetime = movetime % 3000;
-//                    }
-//                }
-//            } else if (huntMode) {
-//                if(hunttime == 0) {
-//                    lastvelocity = this.getVelocity().copy();
-//                    lastposition = this.getPosition().copy();
-//                    lasttime = movetime;
-//                }
-//                hunttime += delta;
-//                if(hunttime >= 3000) {
-//                   huntMode = false;
-//                   waitMode = true;
-//                } else {
-//                    this.getVelocity().x = p.getVelocity().x;
-//                    this.getVelocity().x *= calcPlayerDirection(p).x;
-//                }
-//            } else if (waitMode) {
-//                if(Math.abs(this.getPosition().x - lastposition.x) < 1f) {
-//                    normalMode = true;
-//                    waitMode = false;
-//                    hunttime = 0;
-//                    this.movetime = lasttime;
-//                    this.getVelocity().x = this.lastvelocity.x;
-//                    this.getPosition().x = this.lastposition.x;
-//                 } else {
-//                     this.getVelocity().x = Math.abs(lastvelocity.x) * (new Vector2f(lastposition.x-this.getPosition().x, 0.0f).normalise().x);
-//                 }
-//            }
-//        }
-        MoveLR();
+        p = w.getAstronaut();
+        System.out.println(normalMode + "     " + huntMode + "    " + waitMode);
+        if (p != null) {
+            // float dt = delta / 1000.f;
+            movetime += delta;
+            // TODO clamp dt if dt > 1/60.f ?
+            if (!intelligence) {
+                if (movetime <= 1000) {
+                    if (moveLR) {
+                        setVelocityX(speed);
+                    } else {
+                        setVelocityX(-speed);
+                    }
+                } else {
+                    movetime %= 1000;
+                    if (moveLR) {
+                        moveLR = false;
+                    } else {
+                        moveLR = true;
+                    }
+                }
+            } else {
+                if (normalMode) {
+                    if (calcPlayerDistance(p) < 300) {
+                        normalMode = false;
+                        huntMode = true;
+                    } else {
+                        if (movetime <= 3000) {
+                            if (moveLR) {
+                                setVelocityX(speed);
+                            } else {
+                                setVelocityX(-speed);
+                            }
+                        } else {
+                            movetime %= 3000;
+                            if (moveLR) {
+                                moveLR = false;
+                            } else {
+                                moveLR = true;
+                            }
+                        }
+                    }
+                } else if (huntMode) {
+                    if (hunttime == 0) {
+                        lastposition = this.getPosition().copy();
+                        lasttime = movetime;
+                    }
+                    hunttime += delta;
+                    if (hunttime > 3000) {
+                        huntMode = false;
+                        waitMode = true;
+                    } else {
+                        this.setVelocityX(speed * calcPlayerDirection(p).x);
+                    }
+                } else if (waitMode) {
+                    if (Math.abs(this.getPosition().x - lastposition.x) < 1f) {
+                        normalMode = true;
+                        waitMode = false;
+                        hunttime = 0;
+                        this.movetime = lasttime;
+                        if (moveLR) {
+                            setVelocityX(speed);
+                        } else {
+                            setVelocityX(-speed);
+                        }
+                        this.getPosition().x = this.lastposition.x;
+                    } else {
+                        setVelocityX(speed
+                                * (new Vector2f(lastposition.x
+                                        - this.getPosition().x, 0.0f)
+                                        .normalise().x));
+                    }
+                }
+            }
+        }
     }
-    
+
     private void MoveLR() {
         if (movetime <= 1000) {
             if (moveLR) {
                 setVelocityX(speed);
-            }
-            else {
+            } else {
                 setVelocityX(-speed);
             }
-        }
-        else {
+        } else {
             movetime %= 1000;
             if (moveLR) {
                 moveLR = false;
-            }
-            else {
+            } else {
                 moveLR = true;
             }
         }
     }
-    
+
     private Vector2f calcPlayerDirection(Player player) {
         Vector2f direction = new Vector2f();
         direction = calcPlayerPosition(player);
         direction.normalise();
         return direction;
     }
+
     private float calcPlayerDistance(Player player) {
         Vector2f direction = new Vector2f();
         direction = calcPlayerPosition(player);
-        return (float) Math.sqrt((direction.x*direction.x)+(direction.y*direction.y));
+        return (float) Math.sqrt((direction.x * direction.x)
+                + (direction.y * direction.y));
     }
+
     private Vector2f calcPlayerPosition(Player player) {
         Vector2f direction = new Vector2f();
-        Vector2f position = getPosition();
-        direction.x = player.getPosition().x - position.x;
-        direction.y = player.getPosition().y - position.y;
+        direction.x = player.getPosition().x - this.getPosition().x;
+        direction.y = player.getPosition().y - this.getPosition().y;
         return direction;
     }
+
     public void setReferences(Astronaut p) {
         this.p = p;
     }
@@ -154,6 +188,6 @@ public class GroundEnemy extends AbstractEnemy {
     @Override
     public void endContact(PhysixObject object) {
         // TODO Auto-generated method stub
-        
+
     }
 }
