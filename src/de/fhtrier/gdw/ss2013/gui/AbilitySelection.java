b8667405c91a0a70c578874b6b78fc30391665cd
Davidwichter <davidwichter@yahoo.de>
2013-07-31 13:07:51 +0000

    rewrote the Class "abilitySelection". Abilities displayed on the so called "abillityweel" fade in and out smoothly now.

--- src/de/fhtrier/gdw/ss2013/gui/AbilitySelection.java
+++ src/de/fhtrier/gdw/ss2013/gui/AbilitySelection.java
@@ -14,70 +14,44 @@ import de.fhtrier.gdw.ss2013.game.world.World;
 
 public class AbilitySelection {
 
-	private Image[] ability;
-	private Vector2f position; // ist hier jeweils der Abstand den die Position
+	private AbilityDisplay[] abilityDisplay;
 	private int selected;
-	private Color key;
 	private World worldinstance;
 
 	public AbilitySelection() {
-
 	}
 
-	public void init(Image ability1, Image ability2, Image ability3,
-			Vector2f position, World worldinstance) {
-
-		ability = new Image[3];
-
-		ability[0] = ability1;
-		ability[1] = ability2;
-		ability[2] = ability3;
-
-		this.position = position.copy();
-
-		key = new Color(255f, 255f, 255f, 0.5f);
-		
-		this.worldinstance = worldinstance;
-		selected = worldinstance.getAlien().getselectedAbility();
-	}	
+	public void init(AbilityDisplay[] abilityDisplay,
+            World worldinstance) 
+	{
+	    this.abilityDisplay = abilityDisplay;
+       
+        this.worldinstance = worldinstance;
+        selected = worldinstance.getAlien().getselectedAbility();
+    }
 	
 	public void update(GameContainer container, StateBasedGame game, int delta) {
-	    selected = worldinstance.getAlien().getselectedAbility();
-	}	
+	    
+	    if (selected != worldinstance.getAlien().getselectedAbility())
+	    {
+	      int selectedOld = selected;
+	      selected = worldinstance.getAlien().getselectedAbility();
+	      
+	      abilityDisplay[selectedOld-1].setActivated(false);
+	      abilityDisplay[selected-1].setActivated(true);
+	    };
+	    
+	    for(AbilityDisplay elem : abilityDisplay)
+        {
+            elem.update(container, game, delta);
+        }
+	}
 	
 	public void render(GameContainer container, StateBasedGame game, Graphics g) {
-
-		if (selected != 1) {
-			ability[0].draw(container.getWidth()
-							- (position.x + ability[0].getWidth()), position.y,
-							key);
-		} else {
-			ability[0].draw(container.getWidth()
-							- (position.x + ability[0].getWidth()), position.y);
-		}
-
-		if (selected != 2) {
-			ability[1].draw(container.getWidth()
-							- (position.x + ability[0].getWidth()), position.y
-							+ ability[0].getHeight() + 20, key);
-		} else {
-			ability[1].draw(container.getWidth()
-							- (position.x + ability[0].getWidth()), position.y
-							+ ability[0].getHeight() + 20);
-		}
-
-		if (selected != 3) {
-			ability[2].draw(container.getWidth()
-							- (position.x + ability[0].getWidth()),
-							position.y + ability[0].getHeight() + 20
-									+ ability[1].getHeight() + 20, key);
-		} else {
-			ability[2].draw(container.getWidth()
-							- (position.x + ability[0].getWidth()),
-							position.y + ability[0].getHeight() + 20
-									+ ability[1].getHeight() + 20);
-		}
-
+	    for(AbilityDisplay elem : abilityDisplay)
+	    {
+	        elem.render(container, game, g);
+	    }
 	}
 
 	public void setSelected(int selected) {
