1dccc2858f1a05a1de1a4ea9115d5deecc724be0
Janina.Noell@gmx.de <Janina.Noell@gmx.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-07-31 15:53:16 +0000

    BigGroundEnemy fixed

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/ground/BigGroundEnemy.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/ground/BigGroundEnemy.java
@@ -1,13 +1,11 @@
 package de.fhtrier.gdw.ss2013.game.world.enemies.ground;
 
-import org.newdawn.slick.Animation;
-
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.world.enemies.GroundEnemy;
 
 public class BigGroundEnemy extends GroundEnemy {
 
-	public BigGroundEnemy(Animation animation) {
+	public BigGroundEnemy() {
 		super(AssetLoader.getInstance().getAnimation("ground_ememy_big"));
 	}
 
