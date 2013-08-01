c1eab2c9eee68fe3d07c53fc28a7e23f417dfd0b
info <info@milgo.de>
2013-08-01 23:36:22 +0000

    aufgeräumt

--- src/de/fhtrier/gdw/ss2013/game/world/objects/GrowPlant.java
+++ src/de/fhtrier/gdw/ss2013/game/world/objects/GrowPlant.java
@@ -1,23 +1,14 @@
 package de.fhtrier.gdw.ss2013.game.world.objects;
 
-import org.newdawn.slick.geom.Vector2f;
-import de.fhtrier.gdw.ss2013.game.EntityCollidable;
 import org.jbox2d.dynamics.contacts.Contact;
 
+import de.fhtrier.gdw.ss2013.game.EntityCollidable;
+
 public class GrowPlant extends EntityCollidable {
-    private GrowPlant next;
-    private Vector2f growVelocity;
-    private int number;
    
     public GrowPlant() {
         
        // setParticle(AssetLoader.getInstance().getParticle("pollen1"));
-    }
-    
-    public void init(GrowPlant n, Vector2f velo, int num) {
-        next = n;
-        growVelocity = velo;
-        number = num;
     }
     
     public void grow() {
