8124c00580195efa343de7a80e09942807e72249
krumholt <krumholt@gmail.com>
2013-08-01 12:35:40 +0000

    level kann ueber settings geaendert werden

--- src/de/fhtrier/gdw/ss2013/game/world/World.java
+++ src/de/fhtrier/gdw/ss2013/game/world/World.java
@@ -69,7 +69,7 @@ public class World {
         particleList.clear();
         
 		try {
-			map = AssetLoader.getInstance().loadMap("run_or_die");
+			map = AssetLoader.getInstance().loadMap(DebugModeStatus.getLevelName());
 			LevelLoader.load(map, entityManager, physicsManager);
 
 			mapRender = new MapRenderer(map);
