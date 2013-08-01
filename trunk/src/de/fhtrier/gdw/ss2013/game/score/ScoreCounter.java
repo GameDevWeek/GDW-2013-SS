4dfab6b6345dc6c6e0db50aaafca7987dc6e6ad6
krumholt@gmail.com <krumholt@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 23:18:55 +0000

    score counter verbessert

--- trunk/src/de/fhtrier/gdw/ss2013/game/score/ScoreCounter.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/score/ScoreCounter.java
@@ -2,15 +2,17 @@ package de.fhtrier.gdw.ss2013.game.score;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Vector2f;
 
 public class ScoreCounter {
 
 	int score;
+	Vector2f position = new Vector2f(300, 7);
 	
 	public void render(Graphics g) {
 		g.setColor(Color.red);
-        g.drawRect(300, 7, 100, 22);
-		g.drawString("Score: " + score, 302, 10);
+        g.drawRect(position.x, position.y, 100, 22);
+		g.drawString("Score: " + score, position.x + 2, position.y + 3);
 	}
 	
 	public void addScore(int score) {
