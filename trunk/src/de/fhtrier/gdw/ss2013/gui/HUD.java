f281de987d86df0b6ebc6ef0739c602c18193ce4
boris.month@googlemail.com <boris.month@googlemail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 15:14:10 +0000

    replaced the rectangular healthbar with a round one (UI - Team)

--- trunk/src/de/fhtrier/gdw/ss2013/gui/HUD.java
+++ trunk/src/de/fhtrier/gdw/ss2013/gui/HUD.java
@@ -35,7 +35,7 @@ public class HUD {
 	    // Init healthbar
 	    healthbar = new Progressbar();
 
-		final Vector2f position = new Vector2f(0, 0);
+		final Vector2f position = new Vector2f(20, 20);
 		final Vector2f size = new Vector2f(240, 40); /////////
 		
 		//final String imagePath = "/res/Dummy_GUIs_Images/";
--- trunk/src/de/fhtrier/gdw/ss2013/gui/Progressbar.java
+++ trunk/src/de/fhtrier/gdw/ss2013/gui/Progressbar.java
@@ -4,10 +4,12 @@
 
 package de.fhtrier.gdw.ss2013.gui;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -21,7 +23,10 @@ public class Progressbar {
 	private Image frame;
 	private Image background;
 	private Image bar;
-
+	private Image adaptedBar;
+	private boolean b = false;
+	
+	private Color colorKey;
 	
 	// Werte für konkreten Balken
 	private float maxValue; // gibt den Maximalwert an
@@ -43,15 +48,25 @@ public class Progressbar {
 		this.frame = frame;
 		this.background = background;
 		this.bar = bar;
+		try {
+            this.adaptedBar = new Image(bar.getWidth(), bar.getHeight()/2);
+        } catch (SlickException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 		this.font = font;
 		this.worldinstance = worldinstance;
 		this.maxValue=worldinstance.getAstronaut().getMaxOxygen();
 		
+		//colorKey = new Color(45,4,200);
+		colorKey = new Color(0.0f, 0.0f,0.0f, 0.0f);
 		bar.setCenterOfRotation(0.f, bar.getHeight()/2);
 		
 		
 		//this.size = size.copy();
 		this.size = new Vector2f(bar.getWidth(), bar.getHeight());
+		
+		b = false;
 	}
 
 	public void update(GameContainer container, StateBasedGame game, int delta) {
@@ -63,18 +78,35 @@ public class Progressbar {
 		    currentPercentValue = 0;
 		
 		filled = size.x * (currentPercentValue / 100);
-
-	}
-
-	public void render(GameContainer container, StateBasedGame game, Graphics g) {
+		
+		//Bar rotieren und adaptedBar aus Bar erzeugen
+		//angeleht an diese Website:
+		//http://ray3k.com/site/tutorials/slick-offscreen-rendering/
 		
 		bar.setRotation(90 * (100-currentPercentValue)/100);
-		bar.draw(position.x, position.y - bar.getHeight()/2);
 		
+		Graphics f = container.getGraphics();
+        f.clear();
+        f.drawImage(bar,position.x,position.y-bar.getHeight()/2);
+        //save the result to the outputImage
+        System.out.println("float: (" + position.x + ", " + position.y + ") Int: (" + (int)position.x + ", " + (int)position.y+ ")");
+        f.copyArea(adaptedBar, (int)position.x, (int)position.y);		
+        f.clear();
+        
+       		
+
+	}
+	
+//	Image creatAdaptedBar(Image bar);
+
+	public void render(GameContainer container, StateBasedGame game, Graphics g) {
+	    //es gilt für alle:	 g=container.getGraphics();
+	    g.setBackground(colorKey);
+	    adaptedBar.draw(position.x, position.y);		
 		
 		CenteredText.draw(position.x + size.x / 2 , position.y + size.y / 2, 
 		                  String.format("%.2f %%", currentPercentValue),font);
-		
+	    
 	}
 
 }
