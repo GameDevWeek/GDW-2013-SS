4df14d61a6f636ac61e49db90a3b3b06605a1b49
sonic.gdw <sonic.gdw@gmail.com>
2013-08-01 14:27:28 +0000

    MenuPage for Highscore added (not completed)

---
+++ src/de/fhtrier/gdw/ss2013/menu/pages/MenuPageHighscore.java
@@ -0,0 +1,19 @@
+package de.fhtrier.gdw.ss2013.menu.pages;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import de.fhtrier.gdw.ss2013.menu.MenuManager;
+import de.fhtrier.gdw.ss2013.menu.MenuPage;
+
+public class MenuPageHighscore extends MenuPage {
+
+    public MenuPageHighscore(GameContainer container, StateBasedGame game,
+            MenuManager menuManager, MenuPage parent, String bgImage,
+            String type) throws SlickException {
+        super(container, game, menuManager, parent, bgImage, type);
+        // TODO Auto-generated constructor stub
+    }
+
+}
