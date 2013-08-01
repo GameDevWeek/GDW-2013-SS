a2a1cd818f98d9e82f394d56dc3cebcafcebcfe7
krumholt@gmail.com <krumholt@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 18:38:32 +0000

    score hinzugefuegt

---
+++ trunk/src/de/fhtrier/gdw/ss2013/game/score/ScoreCounter.java
@@ -0,0 +1,23 @@
+package de.fhtrier.gdw.ss2013.game.score;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
+public class ScoreCounter {
+
+	int score;
+	
+	public void render(Graphics g) {
+		g.setColor(Color.red);
+        g.drawRect(300, 7, 100, 22);
+		g.drawString("Score: " + score, 302, 10);
+	}
+	
+	public void addScore(int score) {
+		this.score = score;
+	}
+	
+	public void reset() {
+		score = 0;
+	}
+}
