5345d66e8025bfb970d805c7cf15eef14ca7b170
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 13:09:57 +0000

    killt Gegner

--- trunk/src/de/fhtrier/gdw/ss2013/game/player/PlayerBullet.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/player/PlayerBullet.java
@@ -4,6 +4,7 @@ import org.jbox2d.dynamics.contacts.Contact;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 
+import de.fhtrier.gdw.ss2013.constants.PlayerConstants;
 import de.fhtrier.gdw.ss2013.game.Entity;
 import de.fhtrier.gdw.ss2013.game.world.enemies.EnemyBullet;
 import de.fhtrier.gdw.ss2013.game.world.enemies.FlyingEnemy;
@@ -28,7 +29,7 @@ public class PlayerBullet extends EnemyBullet {
     public void beginContact(Contact contact) {
         Entity other = getOtherEntity(contact);
         if (other instanceof FlyingEnemy) {
-            //((FlyingEnemy) other).reduceHealth(PlayerConstants.BULLET_DAMAGE);
+            ((FlyingEnemy) other).reduceHealth(PlayerConstants.BULLET_DAMAGE);
             this.livetime = 0;
         }
     }
