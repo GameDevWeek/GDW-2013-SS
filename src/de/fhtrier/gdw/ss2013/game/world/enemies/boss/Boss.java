cf80bb1256f94bec7713e2d10fadb0082275cae9
hottehead <hottehead@googlemail.com>
2013-08-01 12:59:59 +0000

    boss angelegt

---
+++ src/de/fhtrier/gdw/ss2013/game/world/enemies/boss/Boss.java
@@ -0,0 +1,29 @@
+package de.fhtrier.gdw.ss2013.game.world.enemies.boss;
+
+import org.jbox2d.dynamics.contacts.Contact;
+import org.newdawn.slick.Animation;
+
+import de.fhtrier.gdw.ss2013.game.world.enemies.AbstractEnemy;
+
+public class Boss extends AbstractEnemy{
+
+	public Boss (Animation moveToRightAnimation) {
+		super(moveToRightAnimation);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void beginContact (Contact object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endContact (Contact object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
