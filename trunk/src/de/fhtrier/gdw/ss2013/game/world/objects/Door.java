31ecfb9d92dd1c8c34e9a0c7c5c72187543ebacd
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 22:26:04 +0000

    Door funktioniert besser, aber immer noch schlecht

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/objects/Door.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/objects/Door.java
@@ -8,25 +8,24 @@
 
 package de.fhtrier.gdw.ss2013.game.world.objects;
 
-import org.newdawn.slick.Color;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.util.Log;
+import org.newdawn.slick.Image;
 
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.Entity;
-import de.fhtrier.gdw.ss2013.game.filter.ActivatableByAstronaut;
 import de.fhtrier.gdw.ss2013.game.filter.Interactable;
 
 public class Door extends Entity implements Interactable {
 
     private boolean open;
-    private long lastOpened;
+    private Image closedImg;
+    private Image openImg;
 
     public Door() {
-        super(AssetLoader.getInstance().getImage("door_closed"));
-        initialize();
+        super();
+        closedImg = AssetLoader.getInstance().getImage("door_closed");
+        openImg = AssetLoader.getInstance().getImage("door_open");
+        setImage(closedImg);
+        // TODO: mach den Tooltip bitte weg
     }
     
     @Override
@@ -38,25 +37,18 @@ public class Door extends Entity implements Interactable {
         if (!open ) {
             open = true;
             physicsObject.setActive(false);
+            setImage(openImg);
         }
-
     }
 
     public void deactivate() {
-
+    	if (open) {
+    		open = false;
+    		physicsObject.setActive(true);
+    		setImage(closedImg);
+    	}
     }
 
-    @Override
-   public void render (GameContainer container, Graphics g) throws SlickException {
-   	if(open){
-   		Color filter = new Color(Color.orange);
-         g.drawImage(img, getPosition().x - (img.getWidth() / 2),
-            getPosition().y - (img.getHeight() / 2),filter);
-   	}else {
-         g.drawImage(img, getPosition().x - (img.getWidth() / 2),
-            getPosition().y - (img.getHeight() / 2));
-		}
-   }
     public boolean isOpen() {
         return open;
     }
