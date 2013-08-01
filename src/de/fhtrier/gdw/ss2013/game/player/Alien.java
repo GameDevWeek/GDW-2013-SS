78e62e0c945025ea1389a5455fdd3c7a80621733
info <info@milgo.de>
2013-08-01 12:46:01 +0000

    Schuss wird in richtige Richtung geschossen

--- src/de/fhtrier/gdw/ss2013/game/player/Alien.java
+++ src/de/fhtrier/gdw/ss2013/game/player/Alien.java
@@ -20,203 +20,199 @@ import de.fhtrier.gdw.ss2013.physix.PhysixManager;
 
 public class Alien extends Player implements AlienController {
 
-    private int selectedAbility;
-    private float mana;
-    private float maxMana;
-    private final Vector2f cursor = new Vector2f();
-    private GameContainer container;
+	private int selectedAbility;
+	private float mana;
+	private float maxMana;
+	private final Vector2f cursor = new Vector2f();
+	private GameContainer container;
 
-    private EntityManager entityManager = World.getInstance()
-            .getEntityManager();
-    // telekinese values
-    private Camera camera = World.getInstance().getCamera();
-    private Box currentSelectedBox = null;
-    private float selectionRadius = 50;
-    private final Vector2f oldcursor = new Vector2f();
-    private final Vector2f dragDirection = new Vector2f();
-    
-    private long lastShotTime = 0;
+	private EntityManager entityManager = World.getInstance().getEntityManager();
+	// telekinese values
+	private Camera camera = World.getInstance().getCamera();
+	private Box currentSelectedBox = null;
+	private float selectionRadius = 50;
+	private final Vector2f oldcursor = new Vector2f();
+	private final Vector2f dragDirection = new Vector2f();
 
-    public Alien() {
-        setAnimation(AssetLoader.getInstance().getAnimation("alien_standing")); // Alien
-                                                                                // does
-                                                                                // NOT
-                                                                                // have
-                                                                                // different
-                                                                                // movestates!
-                                                                                // byRobin
-        selectedAbility = 1;
-        maxMana = 0.0f;
-        mana = maxMana;
-    }
+	private long lastShotTime = 0;
 
-    public void setContainer(GameContainer container) {
-        this.container = container;
-    }
+	public Alien() {
+		setAnimation(AssetLoader.getInstance().getAnimation("alien_standing"));
+		// Alien does NOT have different movestates! byRobin
+		selectedAbility = 1;
+		maxMana = 0.0f;
+		mana = maxMana;
+	}
 
-    public float getMana() {
-        return mana;
-    }
+	public void setContainer(GameContainer container) {
+		this.container = container;
+	}
 
-    public void setMana(float mana) {
-        this.mana = mana;
-    }
+	public float getMana() {
+		return mana;
+	}
 
-    public float getMaxMana() {
-        return maxMana;
-    }
+	public void setMana(float mana) {
+		this.mana = mana;
+	}
 
-    public void setMaxMana(float maxMana) {
-        this.maxMana = maxMana;
-    }
+	public float getMaxMana() {
+		return maxMana;
+	}
 
-    @Override
-    public void shoot() {
-    	if (System.currentTimeMillis() > lastShotTime + PlayerConstants.SHOTDELAY) {
-	        Log.debug("shooting");
-	
-	        EntityManager entityManager = World.getInstance().getEntityManager();
-			PhysixManager physicsManager = World.getInstance().getPhysicsManager();
+	public void setMaxMana(float maxMana) {
+		this.maxMana = maxMana;
+	}
+
+	@Override
+	public void shoot() {
+		if (System.currentTimeMillis() > lastShotTime + PlayerConstants.SHOTDELAY) {
+			Vector2f playerPos;
+			if (World.getInstance().getAstronaut().isCarryAlien()) {
+				playerPos = World.getInstance().getAstronaut().getPosition(); // notwendig, weil alienPos net stimmt
+			}
+			else {
+				playerPos = getPosition();
+			}
+			Vector2f playerScreenVector = World.getInstance().worldToScreenPosition(playerPos);
+			Vector2f shootDirection = new Vector2f(cursor.sub(playerScreenVector));
+			shootDirection = shootDirection.normalise().scale(PlayerConstants.BULLET_SPEED);
 			
+			Log.debug("shooting");
+
+			EntityManager entityManager = World.getInstance().getEntityManager();
+			PhysixManager physicsManager = World.getInstance().getPhysicsManager();
+
 			// Dirty hack to get width and height of an Meteorite
 			int width = AssetLoader.getInstance().getImage("boltEnemy").getWidth();
 			int height = AssetLoader.getInstance().getImage("boltEnemy").getHeight();
-			
+
 			// Create a meteorite entity
 			Entity entity = entityManager.createEntity(PlayerBullet.class);
-	        PhysixBox box = new PhysixBox(physicsManager, getAstronaut().getPosition().x, getAstronaut().getPosition().y, width, height,
-	                BodyType.KINEMATIC, 1, 0.5f, true);
-	        entity.setPhysicsObject(box);
-	        entity.setVelocity(new Vector2f(5f,0));
-	        
-	        lastShotTime = System.currentTimeMillis();
-    	}
-    }
+			PhysixBox box = new PhysixBox(physicsManager, getAstronaut().getPosition().x,
+					getAstronaut().getPosition().y, width, height, BodyType.KINEMATIC, 1, 0.5f, true);
+			entity.setPhysicsObject(box);
+			entity.setVelocity(shootDirection);
 
-    private Astronaut getAstronaut() {
+			lastShotTime = System.currentTimeMillis();
+		}
+	}
+
+	private Astronaut getAstronaut() {
 		return World.getInstance().getAstronaut();
 	}
 
 	@Override
-    public void nextAbility() {
-        selectedAbility = (selectedAbility % 3) + 1;
+	public void nextAbility() {
+		selectedAbility = (selectedAbility % 3) + 1;
 
-        Log.debug("rotate ability");
-    }
+		Log.debug("rotate ability");
+	}
 
-    @Override
-    public void previousAbility() {
+	@Override
+	public void previousAbility() {
 
-        /*
-         * if (selectedAbility > 0){ selectedAbility--; }else{ selectedAbility =
-         * 3; }
-         */
+		/*
+		 * if (selectedAbility > 0){ selectedAbility--; }else{ selectedAbility =
+		 * 3; }
+		 */
 
-        selectedAbility = ((selectedAbility + 1) % 3) + 1;
+		selectedAbility = ((selectedAbility + 1) % 3) + 1;
 
-    }
+	}
 
-    public int getselectedAbility() {
-        return selectedAbility;
-    }
+	public int getselectedAbility() {
+		return selectedAbility;
+	}
 
-    @Override
-    public void useAbility() {
-        Log.debug("using ability");
+	@Override
+	public void useAbility() {
+		Log.debug("using ability");
 
-        switch (selectedAbility) {
-        case 1:
-            // telekinese
-            if (currentSelectedBox == null) {
-                for (Entity e : entityManager.getClosestEntitiesAtPosition(
-                        cursor.add(new Vector2f(camera.getOffsetX(), camera
-                                .getOffsetY())), selectionRadius)) {
-                    System.out.println(e.getName());
-                    if (e instanceof Box) {
-                        currentSelectedBox = (Box) e;
-                    }
-                }
+		switch (selectedAbility) {
+		case 1:
+			// telekinese
+			if (currentSelectedBox == null) {
+				for (Entity e : entityManager.getClosestEntitiesAtPosition(
+						cursor.add(new Vector2f(camera.getOffsetX(), camera.getOffsetY())), selectionRadius)) {
+					System.out.println(e.getName());
+					if (e instanceof Box) {
+						currentSelectedBox = (Box) e;
+					}
+				}
 
-            } else {
-                currentSelectedBox = null;
-            }
-            break;
-        }
-    }
+			}
+			else {
+				currentSelectedBox = null;
+			}
+			break;
+		}
+	}
 
-    @Override
-    public void setCursor(int x, int y) {
-        cursor.set(Math.min(container.getWidth(), Math.max(0, x)),
-                Math.min(container.getHeight(), Math.max(0, y)));
-        // TODO Auto-generated method stub
-        // Log.debug("target direction");
-        // setZustand("animtest");
-    }
+	@Override
+	public void setCursor(int x, int y) {
+		cursor.set(Math.min(container.getWidth(), Math.max(0, x)), Math.min(container.getHeight(), Math.max(0, y)));
+		// TODO Auto-generated method stub
+		// Log.debug("target direction");
+		// setZustand("animtest");
+	}
 
-    @Override
-    public void update(GameContainer container, int delta)
-            throws SlickException {
-        // TODO Auto-generated method stub
-        super.update(container, delta);
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		super.update(container, delta);
 
-        // if (World.getInstance().getAstronaut().isCarryAlien() == true) {
-        // this.setPosition(World.getInstance().getAstronaut().getPosition().x,
-        // (World.getInstance().getAstronaut().getPosition().y));
-        // }
+		// if (World.getInstance().getAstronaut().isCarryAlien() == true) {
+		// this.setPosition(World.getInstance().getAstronaut().getPosition().x,
+		// (World.getInstance().getAstronaut().getPosition().y));
+		// }
 
-        switch (selectedAbility) {
-        case 1:
-            // telekinese
-            if (currentSelectedBox != null) {
-                dragDirection.x = cursor.x + camera.getOffsetX()
-                        - currentSelectedBox.getPosition().x;
-                dragDirection.y = cursor.y + camera.getOffsetY()
-                        - currentSelectedBox.getPosition().y;
-                currentSelectedBox.setVelocity(dragDirection);
-                if (currentSelectedBox.isPlayerOnBox()) {
-                    currentSelectedBox.setVelocity(new Vector2f(0, -10));
-                    currentSelectedBox = null;
-                }
-            }
-        }
-    }
+		switch (selectedAbility) {
+		case 1:
+			// telekinese
+			if (currentSelectedBox != null) {
+				dragDirection.x = cursor.x + camera.getOffsetX() - currentSelectedBox.getPosition().x;
+				dragDirection.y = cursor.y + camera.getOffsetY() - currentSelectedBox.getPosition().y;
+				currentSelectedBox.setVelocity(dragDirection);
+				if (currentSelectedBox.isPlayerOnBox()) {
+					currentSelectedBox.setVelocity(new Vector2f(0, -10));
+					currentSelectedBox = null;
+				}
+			}
+		}
+	}
 
-    @Override
-    public void render(GameContainer container, Graphics g)
-            throws SlickException {
-        Astronaut astronaut = World.getInstance().getAstronaut();
-        // Just render alien if astronaut does not carry the alien
-        if (astronaut != null && !astronaut.isCarryAlien()) {
-            super.render(container, g);
-        }
-    }
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		Astronaut astronaut = World.getInstance().getAstronaut();
+		// Just render alien if astronaut does not carry the alien
+		if (astronaut != null && !astronaut.isCarryAlien()) {
+			super.render(container, g);
+		}
+	}
 
-    @Override
-    public void cursorLeft(float scale) {
-        cursor.x = Math.min(container.getWidth(),
-                Math.max(0, cursor.x - 10.0f * scale));
-    }
+	@Override
+	public void cursorLeft(float scale) {
+		cursor.x = Math.min(container.getWidth(), Math.max(0, cursor.x - 10.0f * scale));
+	}
 
-    @Override
-    public void cursorRight(float scale) {
-        cursor.x = Math.min(container.getWidth(),
-                Math.max(0, cursor.x + 10.0f * scale));
-    }
+	@Override
+	public void cursorRight(float scale) {
+		cursor.x = Math.min(container.getWidth(), Math.max(0, cursor.x + 10.0f * scale));
+	}
 
-    @Override
-    public void cursorUp(float scale) {
-        cursor.y = Math.min(container.getHeight(),
-                Math.max(0, cursor.y - 10.0f * scale));
-    }
+	@Override
+	public void cursorUp(float scale) {
+		cursor.y = Math.min(container.getHeight(), Math.max(0, cursor.y - 10.0f * scale));
+	}
 
-    @Override
-    public void cursorDown(float scale) {
-        cursor.y = Math.min(container.getHeight(),
-                Math.max(0, cursor.y + 10.0f * scale));
-    }
+	@Override
+	public void cursorDown(float scale) {
+		cursor.y = Math.min(container.getHeight(), Math.max(0, cursor.y + 10.0f * scale));
+	}
 
-    public Vector2f getCursor() {
-        return cursor;
-    }
+	public Vector2f getCursor() {
+		return cursor;
+	}
 
 }
