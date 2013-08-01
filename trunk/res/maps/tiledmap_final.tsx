73dda4325fb0f309dd0cbb72aaea77de3d2cbbf3
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 13:51:09 +0000

    "Final" Tileset und mehr

--- trunk/res/maps/tiledmap_final.tsx
+++ trunk/res/maps/tiledmap_final.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiledmap_final" tilewidth="32" tileheight="32">
- <image source="tiledmap_final.png" width="289" height="290"/>
+<tileset name="tiledmap_final" tilewidth="64" tileheight="64">
+ <image source="tiledmap_final.png" width="1600" height="1600"/>
 </tileset>
