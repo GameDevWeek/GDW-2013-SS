68628d8140dc21b3e2816c275217bc1c6a453b3d
Jeremias.Boos <Jeremias.Boos@gmx.de>
2013-08-01 10:08:49 +0000

    No Superman :(

--- src/de/fhtrier/gdw/ss2013/game/world/objects/Box.java
+++ src/de/fhtrier/gdw/ss2013/game/world/objects/Box.java
@@ -1,10 +1,14 @@
 package de.fhtrier.gdw.ss2013.game.world.objects;
 
-import org.newdawn.slick.geom.Vector2f;
+import org.jbox2d.dynamics.Fixture;
+import org.jbox2d.dynamics.contacts.Contact;
 
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.Entity;
+import de.fhtrier.gdw.ss2013.game.EntityCollidable;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
+import de.fhtrier.gdw.ss2013.game.player.Player;
+import de.fhtrier.gdw.ss2013.physix.PhysixObject;
 
 /**
  * Box class
@@ -12,14 +16,52 @@ import de.fhtrier.gdw.ss2013.game.player.Astronaut;
  * @author Kevin, Georg
  * 
  */
-public class Box extends Entity {
-	
-	public Box() {
-		super(AssetLoader.getInstance().getImage("box"));
-	}
+public class Box extends EntityCollidable {
 
-	public void onCollision(Entity e) {
-		if (e instanceof Astronaut) {
-		}
-	}
+    private int isPlayerOnMe = 0;
+
+    public Box() {
+        super(AssetLoader.getInstance().getImage("box"));
+    }
+
+    public void onCollision(Entity e) {
+        if (e instanceof Astronaut) {
+        }
+    }
+
+    public boolean isPlayerOnBox() {
+        return isPlayerOnMe > 0;
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture a = contact.getFixtureA();
+        Fixture b = contact.getFixtureB();
+        PhysixObject objectA = (PhysixObject) a.getBody().getUserData();
+        PhysixObject objectB = (PhysixObject) b.getBody().getUserData();
+
+        if (objectA.getOwner() instanceof Player) {
+            isPlayerOnMe++;
+        }
+        if (objectB.getOwner() instanceof Player) {
+            isPlayerOnMe++;
+        }
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+        Fixture a = contact.getFixtureA();
+        Fixture b = contact.getFixtureB();
+        PhysixObject objectA = (PhysixObject) a.getBody().getUserData();
+        PhysixObject objectB = (PhysixObject) b.getBody().getUserData();
+
+        if (objectA.getOwner() instanceof Player) {
+            isPlayerOnMe--;
+        }
+        if (objectB.getOwner() instanceof Player) {
+            isPlayerOnMe--;
+        }
+
+    }
 }
