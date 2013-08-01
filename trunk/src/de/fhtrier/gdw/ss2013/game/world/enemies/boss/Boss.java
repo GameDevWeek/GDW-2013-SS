148db197ce8e1b1722f3f9e153437518a566831b
krumholt@gmail.com <krumholt@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-01 14:30:24 +0000

    bosskampf

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/boss/Boss.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/boss/Boss.java
@@ -1,29 +1,137 @@
 package de.fhtrier.gdw.ss2013.game.world.enemies.boss;
 
-import org.jbox2d.dynamics.contacts.Contact;
-import org.newdawn.slick.Animation;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 
-import de.fhtrier.gdw.ss2013.game.world.enemies.AbstractEnemy;
+import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
+import de.fhtrier.gdw.ss2013.game.Entity;
+import de.fhtrier.gdw.ss2013.game.player.Astronaut;
+import de.fhtrier.gdw.ss2013.game.world.World;
 
-public class Boss extends AbstractEnemy{
+public class Boss extends Entity {
 
-	public Boss (Animation moveToRightAnimation) {
-		super(moveToRightAnimation);
-		// TODO Auto-generated constructor stub
+	private Phase phase;
+
+	public Boss() {
+		super(AssetLoader.getInstance().getImage("whale"));
+		phase = new IdlePhase();
 	}
 
 	@Override
-	public void beginContact (Contact object) {
-		// TODO Auto-generated method stub
-		
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		phase.render(g);
 	}
 
 	@Override
-	public void endContact (Contact object) {
-		// TODO Auto-generated method stub
-		
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		phase.update(delta);
 	}
 
-	
+	public void setPhase(Phase newPhase) {
+		phase.leave();
+		phase = newPhase;
+		phase.enter();
+	}
 
+	private abstract class Phase {
+
+		void enter() {
+		};
+
+		abstract void update(int delta);
+
+		abstract void render(Graphics g);
+
+		void leave() {
+		};
+	}
+
+	private class IdlePhase extends Phase {
+		private int timer = 3000;
+
+		@Override
+		void enter() {
+			timer = 3000;
+		}
+
+		@Override
+		void update(int delta) {
+			timer -= delta;
+			if (timer < 0) {
+				setPhase(new LoadingPhase());
+			}
+		}
+
+		@Override
+		void render(Graphics g) {
+			float x = getPosition().x - (img.getWidth() / 2);
+			float y = -110 + getPosition().y - (img.getHeight() / 2);
+			g.drawImage(img, x, y);
+		}
+	}
+
+	private class LoadingPhase extends Phase {
+		private int timer;
+
+		@Override
+		void enter() {
+			timer = 1000;
+		}
+
+		@Override
+		void update(int delta) {
+			timer -= delta;
+			if (timer < 0) {
+				setPhase(new AttackingPhase());
+			}
+		}
+
+		@Override
+		void render(Graphics g) {
+			float x = getPosition().x - (img.getWidth() / 2);
+			float y = -110 + getPosition().y - (img.getHeight() / 2);
+			x += (Math.random() * 51) - 25;
+			y += (Math.random() * 51) - 25;
+			g.drawImage(img, x, y);
+		}
+
+	}
+
+	private class AttackingPhase extends Phase {
+		private int timer;
+		Vector2f target;
+		private static final float ATTACKSPEED = 5000;
+
+		@Override
+		void enter() {
+			timer = 500;
+			Astronaut astronaut = World.getInstance().getAstronaut();
+			target = astronaut.getPosition().sub(getPosition()).normalise()
+					.scale(ATTACKSPEED);
+		}
+
+		@Override
+		void update(int delta) {
+			timer -= delta;
+			if (timer < 0) {
+				setPhase(new IdlePhase());
+			}
+		}
+
+		@Override
+		void render(Graphics g) {
+			float x = getPosition().x - (img.getWidth() / 2);
+			float y = -110 + getPosition().y - (img.getHeight() / 2);
+			g.drawImage(img, x, y);
+		}
+
+		@Override
+		void leave() {
+			setVelocity(target);
+		}
+	}
 }
