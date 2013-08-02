443c72323e55e4f3f1d9c260f4b953dad35b7070
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-02 09:26:38 +0000

    Bosstile

--- trunk/res/maps/entity_tileset.tsx
+++ trunk/res/maps/entity_tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="entity_tileset" tilewidth="64" tileheight="64">
- <image source="entity_tileset.png" width="192" height="384"/>
+ <image source="entity_tileset.png" width="192" height="448"/>
  <tile id="0">
   <properties>
    <property name="type" value="start"/>
@@ -8,7 +8,7 @@
  </tile>
  <tile id="1">
   <properties>
-   <property name="type" value="end"/>
+   <property name="type" value="boss"/>
   </properties>
  </tile>
  <tile id="2">
