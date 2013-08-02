98c7ec9f0b484b7f3af2041057027d3a8697fdaa
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-02 09:37:49 +0000

    WinningZone unterstützt Levelchange

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/zones/WinningZone.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/zones/WinningZone.java
@@ -1,16 +1,39 @@
 package de.fhtrier.gdw.ss2013.game.world.zones;
 
 import org.jbox2d.dynamics.contacts.Contact;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
 
 import de.fhtrier.gdw.ss2013.MainGame;
 import de.fhtrier.gdw.ss2013.game.Entity;
 import de.fhtrier.gdw.ss2013.game.EntityCollidable;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
+import de.fhtrier.gdw.ss2013.game.world.World;
 
 public class WinningZone extends EntityCollidable {
 	
+	private String newLevel;
+	private boolean init = false;
+	
 	public WinningZone() {
 		super();
+	}
+	
+	@Override
+	public void initialize() {
+		super.initialize();
+	}
+	
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+		super.update(gc, delta);
+		
+		if (!init) {
+			if (getProperties().getProperty("target") != null) {
+				newLevel = getProperties().getProperty("target");
+			}
+			init = true;
+		}
 	}
 
 	@Override
@@ -18,9 +41,19 @@ public class WinningZone extends EntityCollidable {
 		Entity other = getOtherEntity(contact);
         if (other instanceof Astronaut) {
             if (((Astronaut) other).isCarryAlien()) {
-            	MainGame.changeState(MainGame.WINSTATE);
+            	handleWin();
             }
         }
+	}
+	
+	private void handleWin() {
+		if (newLevel == null) {
+			MainGame.changeState(MainGame.WINSTATE);
+		}
+		else {
+			World.getInstance().setLevelName(newLevel);
+			World.getInstance().shallBeReseted(true);
+		}
 	}
 
 	@Override
