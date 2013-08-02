31a755aefa43d2934e6b4ce2cf8d7ec58bfbedb5
hottehead@googlemail.com <hottehead@googlemail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-02 13:48:34 +0000

    superjump super implementiert!"

--- trunk/src/de/fhtrier/gdw/ss2013/game/player/PlayerState.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/player/PlayerState.java
@@ -4,5 +4,5 @@ package de.fhtrier.gdw.ss2013.game.player;
 /** Contains all movement states from the astronaut.
  * @author Robin */
 public enum PlayerState {
-	standing, walking, jumping, falling, action,superjump
+	standing, walking, jumping, falling, action,superjump_start,superjump,superjump_end
 }
