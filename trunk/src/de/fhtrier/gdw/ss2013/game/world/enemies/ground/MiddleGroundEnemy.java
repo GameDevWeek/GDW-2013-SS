29b0bf55f3d2296c5c8aca1df82aab412fc600d3
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-02 14:53:25 +0000

    GroundEnemy aufgeräumt

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/ground/MiddleGroundEnemy.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/ground/MiddleGroundEnemy.java
@@ -11,7 +11,6 @@ public class MiddleGroundEnemy extends GroundEnemy {
 
 	private static String getRandomImageString() {
 		int rand = (int) (Math.random() * 2);
-		System.out.println(rand);
 		
 		switch (rand) {
 		case 1:
