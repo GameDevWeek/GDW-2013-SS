a6c5c8dd2021645407c09896e45c61f00827ac8f
info@milgo.de <info@milgo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-02 10:34:09 +0000

    Boss kriegt korrektes Physix-Object

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/boss/AbstractBoss.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/boss/AbstractBoss.java
@@ -1,15 +1,17 @@
 package de.fhtrier.gdw.ss2013.game.world.enemies.boss;
 
+import org.jbox2d.dynamics.BodyType;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Vector2f;
 
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
+import de.fhtrier.gdw.ss2013.assetloader.infos.GameDataInfo.WorldInfo;
 import de.fhtrier.gdw.ss2013.game.Entity;
-import de.fhtrier.gdw.ss2013.game.player.Astronaut;
 import de.fhtrier.gdw.ss2013.game.world.World;
+import de.fhtrier.gdw.ss2013.physix.PhysixBox;
+import de.fhtrier.gdw.ss2013.physix.PhysixObject;
 
 public abstract class AbstractBoss extends Entity {
 
@@ -17,6 +19,23 @@ public abstract class AbstractBoss extends Entity {
 
 	public AbstractBoss(Image image) {
 		super(image);
+	}
+	
+	@Override
+	protected void initialize() {
+	    super.initialize();	    
+	    // hier existiert ein PhysixObject, das aber eine falsche Breite und Höhe hat!	
+		float x = physicsObject.getPosition().x;
+		float y = physicsObject.getPosition().y;
+		float width = getImage().getWidth();
+		float height = getImage().getHeight();
+		WorldInfo worldInfo = AssetLoader.getInstance().getGameData().world;
+	    
+		PhysixObject box = new PhysixBox(World.getInstance().getPhysicsManager(), x, y, width, height,
+				BodyType.DYNAMIC, worldInfo.density, worldInfo.friction,
+				false);
+		physicsObject.removeFromWorld(); // dirty way to remove old physix object
+		setPhysicsObject(box);
 	}
 
 	@Override
