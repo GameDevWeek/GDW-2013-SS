cb08d7857620cbfea341c86b80c04ceccdad7e96
hottehead <hottehead@googlemail.com>
2013-08-02 17:45:25 +0000

    levelauswahl dynamisch

--- src/de/fhtrier/gdw/ss2013/assetloader/AssetLoader.java
+++ src/de/fhtrier/gdw/ss2013/assetloader/AssetLoader.java
@@ -7,6 +7,7 @@ package de.fhtrier.gdw.ss2013.assetloader;
 import de.fhtrier.gdw.ss2013.assetloader.infos.CreditsInfo;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 import org.newdawn.slick.AngelCodeFont;
 import org.newdawn.slick.Animation;
@@ -47,21 +48,21 @@ public class AssetLoader {
 
 	private SettingsInfo settings;
 	private GameDataInfo gameStats;
-    private ControlsInfo controlsInfo;
-    private CreditsInfo creditsInfo;
+	private ControlsInfo controlsInfo;
+	private CreditsInfo creditsInfo;
 
 	private static AssetLoader instance;
 
-	public static AssetLoader getInstance() {
+	public static AssetLoader getInstance () {
 		if (instance == null) {
 			instance = new AssetLoader();
 		}
 		return instance;
 	}
 
-	private AssetLoader() {
-	    setupImages("res/json/images.json");
-	    setupMaps("res/json/maps.json");
+	private AssetLoader () {
+		setupImages("res/json/images.json");
+		setupMaps("res/json/maps.json");
 		setupAnimations("res/json/animations.json");
 		setupSounds("res/json/sounds.json");
 		setupMusic("res/json/music.json");
@@ -70,47 +71,47 @@ public class AssetLoader {
 		setupSettings("res/json/settings.json");
 		setupInfos("res/json/infos.json");
 		setupGameStats("res/json/gameData.json");
-        setupControls("res/json/controls.json");
-        setupCredits("res/json/credits.json");
+		setupControls("res/json/controls.json");
+		setupCredits("res/json/credits.json");
 	}
 
 	// BACKSLASH CHECK
 	// ///////////////////////////////////////////////////////////////
 
-	private void checkForBackslashes(String filename) {
+	private void checkForBackslashes (String filename) {
 		if (filename.contains("\\\\")) {
-			throw new IllegalArgumentException(
-					"You shall not use backslashes for paths! Check the JSON-files!");
+			throw new IllegalArgumentException("You shall not use backslashes for paths! Check the JSON-files!");
 		}
 	}
 
 	// SETUP
 	// //////////////////////////////////////////////////////////////////////////
 
-	private void setupAnimations(String filename) {
+	private void setupAnimations (String filename) {
 		try {
 			List<AnimationInfo> animInfos = JacksonReader.readList(filename, AnimationInfo.class);
 			for (AnimationInfo animInfo : animInfos) {
 				checkForBackslashes(animInfo.pfad);
 				Image tmpImg = null;
 				try {
-				    tmpImg = new Image("res/animations/"+animInfo.pfad+".png");
-				}catch (Exception e1) {
-				    try {
-	                    tmpImg = new Image("res/animations/dummies/"+animInfo.pfad+".png");
-	                    Log.info("AssetLoader: Lade dummy animation von '" + animInfo.name + "'");
-	                }catch (Exception e2) {
-	                    try {
-	                        tmpImg = new Image("res/animations/error.png");
-	                        Log.warn("AssetLoader: Animation '" + animInfo.name + "' existiert weder in res/animations noch res/animations/dummies. Lade error.png");
-	                    }catch (Exception e3) {
-	                        Log.warn("AssetLoader: Fatal Error (animations) - kann error.png nicht laden.");
-	                        e1.printStackTrace();
-	                        e2.printStackTrace();
-	                        e3.printStackTrace();
-	                    }
-	                } 
-		        }
+					tmpImg = new Image("res/animations/" + animInfo.pfad + ".png");
+				} catch (Exception e1) {
+					try {
+						tmpImg = new Image("res/animations/dummies/" + animInfo.pfad + ".png");
+						Log.info("AssetLoader: Lade dummy animation von '" + animInfo.name + "'");
+					} catch (Exception e2) {
+						try {
+							tmpImg = new Image("res/animations/error.png");
+							Log.warn("AssetLoader: Animation '" + animInfo.name
+								+ "' existiert weder in res/animations noch res/animations/dummies. Lade error.png");
+						} catch (Exception e3) {
+							Log.warn("AssetLoader: Fatal Error (animations) - kann error.png nicht laden.");
+							e1.printStackTrace();
+							e2.printStackTrace();
+							e3.printStackTrace();
+						}
+					}
+				}
 				Animation anim = new Animation();
 				SpriteSheet sheet = new SpriteSheet(tmpImg, tmpImg.getWidth() / animInfo.anzBilder, tmpImg.getHeight());
 				for (int i = 0; i < animInfo.anzBilder; i += 1) {
@@ -123,31 +124,29 @@ public class AssetLoader {
 			e.printStackTrace();
 		}
 	}
-	
-	private void setupControls(String filename) {
-        try {
-            controlsInfo = JacksonReader.read(filename, ControlsInfo.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-	private void setupFonts(String filename) {
+	private void setupControls (String filename) {
 		try {
-			List<FontInfo> fontInfos = JacksonReader.readList(filename,
-					FontInfo.class);
+			controlsInfo = JacksonReader.read(filename, ControlsInfo.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setupFonts (String filename) {
+		try {
+			List<FontInfo> fontInfos = JacksonReader.readList(filename, FontInfo.class);
 			for (FontInfo fontInfo : fontInfos) {
 				checkForBackslashes(fontInfo.file);
 				checkForBackslashes(fontInfo.image);
-				fontMap.put(fontInfo.name, new AngelCodeFont(fontInfo.file,
-						fontInfo.image));
+				fontMap.put(fontInfo.name, new AngelCodeFont(fontInfo.file, fontInfo.image));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void setupGameStats(String filename) {
+	private void setupGameStats (String filename) {
 		try {
 			gameStats = JacksonReader.read(filename, GameDataInfo.class);
 		} catch (Exception e) {
@@ -155,30 +154,31 @@ public class AssetLoader {
 		}
 	}
 
-	private void setupImages(String filename) {
+	private void setupImages (String filename) {
 		try {
 			List<ImageInfo> imageInfos = JacksonReader.readList(filename, ImageInfo.class);
 			for (ImageInfo imageInfo : imageInfos) {
 				checkForBackslashes(imageInfo.pfad);
 				Image tmpImg = null;
 				try {
-				    tmpImg = new Image("res/images/"+imageInfo.pfad+".png");
+					tmpImg = new Image("res/images/" + imageInfo.pfad + ".png");
 				} catch (Exception e1) {
-				    try {
-				        tmpImg = new Image("res/images/dummies/"+imageInfo.pfad+".png");
-				        Log.info("AssetLoader: Lade dummy image von '" + imageInfo.name + "'");
-				    } catch (Exception e2) {
-				        try {
-				            tmpImg = imageMap.get("error");
-				            Log.warn("AssetLoader: Image '" + imageInfo.name + "' existiert weder in res/images noch res/images/dummies. Lade error.png");
-				        } catch (Exception e3) {
-				            Log.warn("AssetLoader: Fatal Error (images) - kann error.png nicht laden.");
-				            e1.printStackTrace();
-				            e2.printStackTrace();
-				            e3.printStackTrace();
-				        }
-				    }
-		        }
+					try {
+						tmpImg = new Image("res/images/dummies/" + imageInfo.pfad + ".png");
+						Log.info("AssetLoader: Lade dummy image von '" + imageInfo.name + "'");
+					} catch (Exception e2) {
+						try {
+							tmpImg = imageMap.get("error");
+							Log.warn("AssetLoader: Image '" + imageInfo.name
+								+ "' existiert weder in res/images noch res/images/dummies. Lade error.png");
+						} catch (Exception e3) {
+							Log.warn("AssetLoader: Fatal Error (images) - kann error.png nicht laden.");
+							e1.printStackTrace();
+							e2.printStackTrace();
+							e3.printStackTrace();
+						}
+					}
+				}
 				imageMap.put(imageInfo.name, tmpImg);
 			}
 		} catch (Exception e) {
@@ -186,10 +186,9 @@ public class AssetLoader {
 		}
 	}
 
-	private void setupInfos(String filename) {
+	private void setupInfos (String filename) {
 		try {
-			List<InfoInfo> infoInfos = JacksonReader.readList(filename,
-					InfoInfo.class);
+			List<InfoInfo> infoInfos = JacksonReader.readList(filename, InfoInfo.class);
 			for (InfoInfo infoInfo : infoInfos) {
 				infosMap.put(infoInfo.name, infoInfo.info);
 			}
@@ -198,24 +197,22 @@ public class AssetLoader {
 		}
 	}
 
-	private void setupMaps(String filename) {
+	private void setupMaps (String filename) {
 		try {
-			List<MapInfo> mapInfos = JacksonReader.readList(filename,
-					MapInfo.class);
+			List<MapInfo> mapInfos = JacksonReader.readList(filename, MapInfo.class);
 			for (MapInfo mapInfo : mapInfos) {
 				checkForBackslashes(mapInfo.pfad);
 				mapHashmap.put(mapInfo.name, mapInfo.pfad); // don't load every
-															// map during setup
+				// map during setup
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void setupParticles(String filename) {
+	private void setupParticles (String filename) {
 		try {
-			List<ParticleInfo> particleInfos = JacksonReader.readList(filename,
-					ParticleInfo.class);
+			List<ParticleInfo> particleInfos = JacksonReader.readList(filename, ParticleInfo.class);
 			for (ParticleInfo particleInfo : particleInfos) {
 				checkForBackslashes(particleInfo.pfad);
 				particleMap.put(particleInfo.name, new DynamicParticleSystem(particleInfo.pfad, true));
@@ -226,14 +223,13 @@ public class AssetLoader {
 		}
 	}
 
-	private List<ScoreInfo> setupScore(String filename) {
+	private List<ScoreInfo> setupScore (String filename) {
 		List<ScoreInfo> scoreInfos = null;
 		try {
 			scoreInfos = JacksonReader.readList(filename, ScoreInfo.class);
 		} catch (Exception e) {
 			try {
-				scoreInfos = JacksonReader.readList(
-						"res/json/scores/default.json", ScoreInfo.class);
+				scoreInfos = JacksonReader.readList("res/json/scores/default.json", ScoreInfo.class);
 			} catch (Exception e1) {
 				e1.printStackTrace();
 				e.printStackTrace();
@@ -242,7 +238,7 @@ public class AssetLoader {
 		return scoreInfos;
 	}
 
-	private void setupSettings(String filename) {
+	private void setupSettings (String filename) {
 		try {
 			settings = JacksonReader.read(filename, SettingsInfo.class);
 		} catch (Exception e) {
@@ -250,7 +246,7 @@ public class AssetLoader {
 		}
 	}
 
-	private void setupSounds(String filename) {
+	private void setupSounds (String filename) {
 		try {
 			List<SoundInfo> soundInfos = JacksonReader.readList(filename, SoundInfo.class);
 			for (SoundInfo soundInfo : soundInfos) {
@@ -262,7 +258,7 @@ public class AssetLoader {
 		}
 	}
 
-	private void setupMusic(String filename) {
+	private void setupMusic (String filename) {
 		try {
 			List<SoundInfo> musicInfos = JacksonReader.readList(filename, SoundInfo.class);
 			for (SoundInfo musicInfo : musicInfos) {
@@ -274,44 +270,44 @@ public class AssetLoader {
 		}
 	}
 
-    private void setupCredits(String filename) {
+	private void setupCredits (String filename) {
 		try {
 			creditsInfo = JacksonReader.read(filename, CreditsInfo.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
+	}
 
 	// GET
 	// //////////////////////////////////////////////////////////////////////////
 
-	public Animation getAnimation(String name) {
+	public Animation getAnimation (String name) {
 		if (animMap.get(name) == null) {
-		    Log.warn("AssetLoader: Animation '" + name + "' existiert nicht.");
+			Log.warn("AssetLoader: Animation '" + name + "' existiert nicht.");
 			return animMap.get("error").copy();
 		} else {
 			return animMap.get(name).copy();
 		}
 	}
-	
-	public ControlsInfo getControls() {
-        return controlsInfo;
-    }
 
-	public Font getFont(String name) {
-	    if(fontMap.get(name) == null){
-	        Log.warn("AssetLoader: Font '" + name + "' existiert nicht. Du bekommst 'verdana_90'.");
-            return fontMap.get("verdana_90");      
-	    }else{
-	        return fontMap.get(name);
-	    }
+	public ControlsInfo getControls () {
+		return controlsInfo;
 	}
 
-	public GameDataInfo getGameData() {
+	public Font getFont (String name) {
+		if (fontMap.get(name) == null) {
+			Log.warn("AssetLoader: Font '" + name + "' existiert nicht. Du bekommst 'verdana_90'.");
+			return fontMap.get("verdana_90");
+		} else {
+			return fontMap.get(name);
+		}
+	}
+
+	public GameDataInfo getGameData () {
 		return gameStats;
 	}
 
-	public Image getImage(String name) {
+	public Image getImage (String name) {
 		Image image = imageMap.get(name);
 		if (image == null) {
 			Log.warn("AssetLoader: Image '" + name + "' existiert nicht.");
@@ -321,68 +317,68 @@ public class AssetLoader {
 		}
 	}
 
-	public String getInfo(String name) {
-	    if(infosMap.get(name) == null){
-	        Log.warn("AssetLoader: Info '" + name + "' existiert nicht.");
-	        return infosMap.get("error");
-	    }else{
-	        return infosMap.get(name);
-	    }	
+	public String getInfo (String name) {
+		if (infosMap.get(name) == null) {
+			Log.warn("AssetLoader: Info '" + name + "' existiert nicht.");
+			return infosMap.get("error");
+		} else {
+			return infosMap.get(name);
+		}
 	}
 
-	/**
-	 * 
-	 * @param name
-	 *            JSON map name
-	 * @return Path to map file
-	 */
-	public String getMapPath(String name) {
+	public Set<String> getMapInfos () {
+		return mapHashmap.keySet();
+	}
+
+	/** @param name JSON map name
+	 * @return Path to map file */
+	public String getMapPath (String name) {
 		return mapHashmap.get(name);
 	}
 
-	public DynamicParticleSystem getParticle(String name) {
+	public DynamicParticleSystem getParticle (String name) {
 		if (particleMap.get(name) == null) {
-		    Log.warn("AssetLoader: Particleanimation '" + name + "' existiert nicht.");
+			Log.warn("AssetLoader: Particleanimation '" + name + "' existiert nicht.");
 			return particleMap.get("error");
 		} else {
 			return particleMap.get(name).clone();
 		}
 	}
 
-	public List<ScoreInfo> getScore(String scoreName) {
+	public List<ScoreInfo> getScore (String scoreName) {
 		return setupScore("res/json/scores/" + scoreName + ".json");
 	}
 
-	public SettingsInfo getSettings() {
+	public SettingsInfo getSettings () {
 		return settings;
 	}
 
-	public Sound getSound(String name) {
-	    if(soundMap.get(name) == null){
-	        Log.warn("AssetLoader: Sound '" + name + "' existiert nicht.");
-	        return soundMap.get("error");
-	    }else{
-	        return soundMap.get(name);
-	    }
+	public Sound getSound (String name) {
+		if (soundMap.get(name) == null) {
+			Log.warn("AssetLoader: Sound '" + name + "' existiert nicht.");
+			return soundMap.get("error");
+		} else {
+			return soundMap.get(name);
+		}
 	}
 
-    public Music getMusic(String name) {
-	    if(musicMap.get(name) == null){
-	        Log.warn("AssetLoader: Music '" + name + "' existiert nicht.");
-	        return musicMap.get("error");
-	    }else{
-	        return musicMap.get(name);
-	    }
-    }
+	public Music getMusic (String name) {
+		if (musicMap.get(name) == null) {
+			Log.warn("AssetLoader: Music '" + name + "' existiert nicht.");
+			return musicMap.get("error");
+		} else {
+			return musicMap.get(name);
+		}
+	}
 
-    public CreditsInfo getCredits() {
-        return creditsInfo;
-    }
+	public CreditsInfo getCredits () {
+		return creditsInfo;
+	}
 
-	//WRITE
+	// WRITE
 	// ///////////////////////////////////////////////////////////////////////
-	
-	public void writeSettings(SettingsInfo settings) {
+
+	public void writeSettings (SettingsInfo settings) {
 		try {
 			JacksonWriter.write("res/json/settings.json", settings);
 		} catch (Exception e) {
@@ -390,10 +386,9 @@ public class AssetLoader {
 		}
 	}
 
-	public void writeScore(String scoreName, List<ScoreInfo> scoreList) {
+	public void writeScore (String scoreName, List<ScoreInfo> scoreList) {
 		try {
-			JacksonWriter.writeList("res/json/scores/" + scoreName + ".json",
-					scoreList);
+			JacksonWriter.writeList("res/json/scores/" + scoreName + ".json", scoreList);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -402,17 +397,12 @@ public class AssetLoader {
 	// LOADMAP
 	// ///////////////////////////////////////////////////////////////////////
 
-	/**
-	 * 
-	 * @param mapname
-	 *            Mapname from JSON-Files
+	/** @param mapname Mapname from JSON-Files
 	 * @return Loaded TiledMap
-	 * @throws Exception
-	 */
-	public TiledMap loadMap(String mapname) throws Exception {
+	 * @throws Exception */
+	public TiledMap loadMap (String mapname) throws Exception {
 		AssetLoader assetLoader = AssetLoader.getInstance();
-		TiledMap map = new TiledMap(assetLoader.getMapPath(mapname),
-				LayerObject.PolyMode.ABSOLUTE);
+		TiledMap map = new TiledMap(assetLoader.getMapPath(mapname), LayerObject.PolyMode.ABSOLUTE);
 		return map;
 	}
 }
--- src/de/fhtrier/gdw/ss2013/menu/pages/MenuPageLevelSelect.java
+++ src/de/fhtrier/gdw/ss2013/menu/pages/MenuPageLevelSelect.java
@@ -1,57 +1,57 @@
+
 package de.fhtrier.gdw.ss2013.menu.pages;
+
+import java.util.Set;
 
 import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.util.Log;
 
 import de.fhtrier.gdw.ss2013.MainGame;
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
+import de.fhtrier.gdw.ss2013.game.world.World;
 import de.fhtrier.gdw.ss2013.menu.IActionListener;
 import de.fhtrier.gdw.ss2013.menu.Label;
 import de.fhtrier.gdw.ss2013.menu.MenuManager;
 import de.fhtrier.gdw.ss2013.menu.MenuPage;
+import de.fhtrier.gdw.ss2013.menu.ToggleButton;
 import de.fhtrier.gdw.ss2013.menu.Widget.Align;
 
 public class MenuPageLevelSelect extends MenuPage {
 
-    public MenuPageLevelSelect(GameContainer container, StateBasedGame game,
-            MenuManager menuManager, MenuPage parent, String bgImage) throws SlickException {
-        super(container, game, menuManager, parent, bgImage, "levelselect");
-        
-        Font font = AssetLoader.getInstance().getFont("verdana_46");
-        
-        float xCenter = MenuManager.MENU_WIDTH / 2.0f;
-        float yCenter = MenuManager.MENU_HEIGHT / 2.0f;
-        
-        float hText = font.getLineHeight() * 1.5f;
-        
-        Label lvlAuswahl = addLeftAlignedLabel("Levelauswahl:", 10, yCenter * 0.25f , font);
-        
-        
-        String[] levels = {"run_or_die", "not_yet_implemented", "you_dont_say"};
-        
-        float space = font.getWidth(lvlAuswahl.text);
-        addLeftAlignedToggleButton(levels, 10 + space + 50, yCenter * 0.25f, font, Align.LEFT);
-        
-        addCenteredButton("GO!", xCenter, yCenter, font, 
-                new IActionListener() { 
-                    @Override
-                    public void onAction() {
-                        MainGame.changeState(MainGame.GAMEPLAYSTATE);
-                    }
-                }
-        );
-        
-        float textHeight = font.getLineHeight();
-        addCenteredButton("zurück", xCenter, MenuManager.MENU_HEIGHT - 1.5f * textHeight, font, 
-                new IActionListener() { 
-                    @Override
-                    public void onAction() {
-                        close();
-                    }
-                }
-        );
-    }
+	public MenuPageLevelSelect (GameContainer container, StateBasedGame game, MenuManager menuManager, MenuPage parent,
+		String bgImage) throws SlickException {
+		super(container, game, menuManager, parent, bgImage, "levelselect");
 
+		Font font = AssetLoader.getInstance().getFont("verdana_46");
+
+		float xCenter = MenuManager.MENU_WIDTH / 2.0f;
+		float yCenter = MenuManager.MENU_HEIGHT / 2.0f;
+
+		float hText = font.getLineHeight() * 1.5f;
+
+		Label lvlAuswahl = addLeftAlignedLabel("Levelauswahl:", 10, yCenter * 0.25f, font);
+
+		Set<String> levels = AssetLoader.getInstance().getMapInfos();
+		float space = font.getWidth(lvlAuswahl.text);
+		final String[] lvls = levels.toArray(new String[levels.size()]);
+		final ToggleButton tb=addLeftAlignedToggleButton(lvls, 10 + space + 50, yCenter * 0.25f, font, Align.LEFT);
+		
+		addCenteredButton("GO!", xCenter, yCenter, font, new IActionListener() {
+			public void onAction () {
+				World.getInstance().setLevelName(lvls[tb.getState()]);
+				World.getInstance().shallBeReseted(true);
+				MainGame.changeState(MainGame.GAMEPLAYSTATE);
+			}
+		});
+
+		float textHeight = font.getLineHeight();
+		addCenteredButton("zurück", xCenter, MenuManager.MENU_HEIGHT - 1.5f * textHeight, font, new IActionListener() {
+			public void onAction () {
+				close();
+			}
+		});
+	}
 }
