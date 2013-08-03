fc2f1dd12f3a174e6b287ee49bc54b4f48f1620e
davidwichter@yahoo.de <davidwichter@yahoo.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-03 13:21:11 +0000

    checkpoint initial commit

---
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/objects/Checkpoint.java
@@ -0,0 +1,58 @@
+package de.fhtrier.gdw.ss2013.game.world.objects;
+
+
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.contacts.Contact;
+
+import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
+import de.fhtrier.gdw.ss2013.constants.MathConstants;
+import de.fhtrier.gdw.ss2013.game.Entity;
+import de.fhtrier.gdw.ss2013.game.EntityCollidable;
+import de.fhtrier.gdw.ss2013.game.player.Astronaut;
+import de.fhtrier.gdw.ss2013.game.world.World;
+
+public class Checkpoint extends EntityCollidable {
+    
+    private float radius;
+   
+   public Checkpoint(){
+       super(AssetLoader.getInstance().getImage("Checkpoint"));
+       radius = 250;
+   }
+   
+   public Checkpoint(float radius){
+       super();
+       this.radius = radius;
+   }
+     
+   public void update(){
+       
+       System.out.println("checkpoint update");
+           
+   }
+
+   @Override
+   public void initPhysics() {
+       createPhysics(BodyType.STATIC, origin.x, origin.y).sensor(true).asBox(
+               initialSize.x, initialSize.y);
+   }
+
+   @Override
+   public void beginContact(Contact contact) {
+
+       Entity other = getOtherEntity(contact);
+       if(other instanceof Astronaut) {
+           Astronaut player = World.getInstance().getAstronaut();
+           player.setOrigin(player.getPosition());
+       }
+    
+   }
+
+   @Override
+   public void endContact(Contact object) {
+    
+    
+
+   }
+
+}
