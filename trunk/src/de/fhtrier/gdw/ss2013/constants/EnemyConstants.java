d4eaea750b4641d0249dad7853500f5138254b36
georg.peter.schaefer@googlemail.com <georg.peter.schaefer@googlemail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-03 20:43:52 +0000

    something fixed with fling enemies don't mind

--- trunk/src/de/fhtrier/gdw/ss2013/constants/EnemyConstants.java
+++ trunk/src/de/fhtrier/gdw/ss2013/constants/EnemyConstants.java
@@ -5,6 +5,7 @@ public class EnemyConstants {
 	public static final float ENEMY_BULLET_DAMAGE = 40f;
 	public static final float ENEMY_SHOOTING_DISTANCE = 1000f;
 	public static final float ENEMY_SHOOTING_INTERVAL = 1500f;
+    public static final float FLYING_DAMAGE = 80f;
 	public static float ENEMY_SPEED = 100f;
 	public static float ENEMY_MAX_RANGE = 400f;
 	public static float ENEMY_MAX_HEIGHT_DIFFERENCE = 125f;
