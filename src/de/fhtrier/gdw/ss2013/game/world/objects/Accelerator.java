5e60f3b3ec3da08740e9752d1eaaf5add84295a0
krumholt <krumholt@gmail.com>
2013-08-03 15:04:30 +0000

    boss verbessert

--- src/de/fhtrier/gdw/ss2013/game/world/objects/Accelerator.java
+++ src/de/fhtrier/gdw/ss2013/game/world/objects/Accelerator.java
@@ -4,6 +4,7 @@ package de.fhtrier.gdw.ss2013.game.world.objects;
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.Entity;
 import de.fhtrier.gdw.ss2013.game.EntityCollidable;
+import de.fhtrier.gdw.ss2013.game.filter.Interactable;
 import de.fhtrier.gdw.ss2013.game.player.Alien;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
 import de.fhtrier.gdw.ss2013.game.world.World;
@@ -11,7 +12,9 @@ import de.fhtrier.gdw.ss2013.game.world.World;
 import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.contacts.Contact;
 
-public class Accelerator extends EntityCollidable {
+public class Accelerator extends EntityCollidable implements Interactable {
+
+	private boolean isActive = false;
 
     public Accelerator() {
     	super(AssetLoader.getInstance().getImage("button_unpressed"));
@@ -36,14 +39,14 @@ public class Accelerator extends EntityCollidable {
     public void beginContact(Contact contact) {
         Entity other = getOtherEntity(contact);
         
-        if (other instanceof Astronaut || other instanceof Alien || other instanceof Box) {
+        if (!isActive && (other instanceof Astronaut || other instanceof Alien || other instanceof Box)) {
             Alien alien = World.getInstance().getAlien();
             if(other == alien.getCurrentSelectedBox()) {
                 alien.dropCurrentSelected();
             }
             
-            float x = properties.getFloat("velocity_x", 0);
-            float y = properties.getFloat("velocity_y", 0);
+            float x = properties.getFloat("velocity_x", 50);
+            float y = properties.getFloat("velocity_y", -5000);
             if(y != 0)
                 other.setVelocityY(y);
             if(x != 0)
@@ -56,4 +59,19 @@ public class Accelerator extends EntityCollidable {
     @Override
     public void endContact(Contact contact) {
     }
+
+	@Override
+	public void activate() {
+		isActive = true;
+	}
+
+	@Override
+	public void deactivate() {
+		isActive = false;
+	}
+
+	@Override
+	public boolean isActive() {
+		return !isActive;
+	}
 }
