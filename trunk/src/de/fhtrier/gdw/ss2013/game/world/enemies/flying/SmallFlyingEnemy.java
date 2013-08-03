fa403471784fc0f2187f30d2fd1f14756f49efdb
Jeremias.Boos@gmx.de <Jeremias.Boos@gmx.de@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-03 12:53:59 +0000

    GegnerSounds

--- trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/flying/SmallFlyingEnemy.java
+++ trunk/src/de/fhtrier/gdw/ss2013/game/world/enemies/flying/SmallFlyingEnemy.java
@@ -2,28 +2,35 @@ package de.fhtrier.gdw.ss2013.game.world.enemies.flying;
 
 import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.world.enemies.FlyingEnemy;
+import de.fhtrier.gdw.ss2013.sound.SoundLocator;
 
 public class SmallFlyingEnemy extends FlyingEnemy {
 
-	public SmallFlyingEnemy() {
-		super(AssetLoader.getInstance().getAnimation(getRandomImage()));
-	}
+    public SmallFlyingEnemy() {
+        super(AssetLoader.getInstance().getAnimation(getRandomImage()));
+    }
 
-	private static String getRandomImage() {
-		String imgString;
-		int rand = (int) (Math.random() * 3);
-		switch (rand) {
-		case 1:
-			imgString = "flying_ememy_small_red";
-			break;
-		case 2:
-			imgString = "flying_ememy_small_green";
-			break;
-		default:
-			imgString = "flying_ememy_small_blue";
-			break;
-		}
-		return imgString;
-	}
+    @Override
+    protected void initialize() {
+        dieSound = SoundLocator.loadSound("fliegengegnertot");
+        super.initialize();
+    }
+
+    private static String getRandomImage() {
+        String imgString;
+        int rand = (int) (Math.random() * 3);
+        switch (rand) {
+        case 1:
+            imgString = "flying_ememy_small_red";
+            break;
+        case 2:
+            imgString = "flying_ememy_small_green";
+            break;
+        default:
+            imgString = "flying_ememy_small_blue";
+            break;
+        }
+        return imgString;
+    }
 
 }
