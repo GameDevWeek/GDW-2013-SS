517175667bfd2abd9b56eb134f15f7be5ed4d0f5
Jeremias.Boos <Jeremias.Boos@gmx.de>
2013-08-04 11:02:38 +0000

    "Fixed" Tilemaps New Door image load system

--- src/de/fhtrier/gdw/ss2013/game/world/objects/Door.java
+++ src/de/fhtrier/gdw/ss2013/game/world/objects/Door.java
@@ -16,57 +16,68 @@ import de.fhtrier.gdw.ss2013.game.filter.Interactable;
 
 public class Door extends Entity implements Interactable {
 
-	private int opened;
-	private Image closedImg;
-	private Image openImg;
+    private int opened;
+    private Image closedImg;
+    private Image openImg;
 
-	public Door() {
-		super();
-		closedImg = AssetLoader.getInstance().getImage("door_closed");
-		openImg = AssetLoader.getInstance().getImage("door_open");
-		setImage(closedImg);
-		opened = 0;
-	}
+    public Door() {
+        super();
+        closedImg = AssetLoader.getInstance().getImage("door_closed");
+        openImg = AssetLoader.getInstance().getImage("door_open");
+        setImage(closedImg);
+        opened = 0;
+    }
 
     @Override
     public boolean isBottomPositioned() {
         return true;
     }
-	
-	@Override
-	protected void initialize() {
-	    super.initialize();	    
-	    
-		float width = closedImg.getWidth();
-		float height = closedImg.getHeight();
+
+    @Override
+    protected void initialize() {
+        super.initialize();
+
+        float width = closedImg.getWidth();
+        float height = closedImg.getHeight();
         setInitialSize(width, height);
-	}
+
+        if (properties != null) {
+            String doorType = properties.getProperty("doorType", "")
+                    .toLowerCase();
+            if (!doorType.isEmpty()) {
+                closedImg = AssetLoader.getInstance().getImage(
+                        doorType + "_door_closed");
+                openImg = AssetLoader.getInstance().getImage(
+                        doorType + "_door_open");
+            }
+        }
+    }
 
     @Override
-	public void activate() {
-		opened++;
-		if (isOpen()) {
-			physicsObject.setActive(false);
-			setImage(openImg);
-		}
-	}
+    public void activate() {
+        opened++;
+        if (isOpen()) {
+            physicsObject.setActive(false);
+            setImage(openImg);
+        }
+    }
 
     @Override
-	public void deactivate() {
-		opened--;
-		if (!isOpen()) {
-			physicsObject.setActive(true);
-			setImage(closedImg);
-		}
-	}
+    public void deactivate() {
+        opened--;
+        if (!isOpen()) {
+            physicsObject.setActive(true);
+            setImage(closedImg);
+        }
+    }
 
-	public boolean isOpen() {
-		return opened > 0;
-	}
+    public boolean isOpen() {
+        return opened > 0;
+    }
 
-	@Override
-	public boolean isActive() {
-		return isOpen();
-	}
+    @Override
+    public boolean isActive() {
+        return isOpen();
+    }
 
 }
