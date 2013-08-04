a2255d62e8903681e0fcb81f06e10b44a3d5b04f
albsi27@gmail.com <albsi27@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-04 11:43:35 +0000

    swamp vg assets tileset

---
+++ trunk/res/maps/assets_swamp.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="assets_swamp" tilewidth="250" tileheight="200">
+ <image source="assets_swamp.png" width="1243" height="404"/>
+</tileset>
