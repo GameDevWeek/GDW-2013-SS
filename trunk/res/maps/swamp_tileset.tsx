52b8530caf7a84ea86e36b12fc64e52d760a71d1
albsi27@gmail.com <albsi27@gmail.com@43f6035e-643c-512f-8ef0-e83f9214d7ef>
2013-08-04 10:29:54 +0000

    swamp_tileset.tsx

---
+++ trunk/res/maps/swamp_tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="swamp_tileset" tilewidth="64" tileheight="64">
+ <image source="swamp_tileset.png" width="800" height="1200"/>
+</tileset>
