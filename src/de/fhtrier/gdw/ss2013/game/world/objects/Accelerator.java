83f905d90e76914a1285a76ad2d0e7b484e23a91
lusito.info <lusito.info@googlemail.com>
2013-08-04 11:50:24 +0000

    better looking accelerators

--- src/de/fhtrier/gdw/ss2013/game/world/objects/Accelerator.java
+++ src/de/fhtrier/gdw/ss2013/game/world/objects/Accelerator.java
@@ -1,21 +1,26 @@
 package de.fhtrier.gdw.ss2013.game.world.objects;
 
 
+import de.fhtrier.gdw.ss2013.assetloader.AssetLoader;
 import de.fhtrier.gdw.ss2013.game.Entity;
 import de.fhtrier.gdw.ss2013.game.EntityCollidable;
+import de.fhtrier.gdw.ss2013.game.filter.Interactable;
 import de.fhtrier.gdw.ss2013.game.player.Alien;
 import de.fhtrier.gdw.ss2013.game.player.Astronaut;
 import de.fhtrier.gdw.ss2013.game.world.World;
 
 import org.jbox2d.dynamics.BodyType;
 import org.jbox2d.dynamics.contacts.Contact;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 public class Accelerator extends EntityCollidable {
 
-	private boolean isActive = true;
+    private Animation animation;
+    private boolean invertAnimation;
+    private boolean invertAnimationUp;
 
     public Accelerator() {
     	super();
@@ -27,6 +32,17 @@ public class Accelerator extends EntityCollidable {
     @Override
     protected void initialize() {
         super.initialize();
+        
+        float x = properties.getFloat("velocity_x", 0);
+        float y = properties.getFloat("velocity_y", 0);
+        if (Math.abs(x) > Math.abs(y)) {
+            animation = AssetLoader.getInstance().getAnimation("accelerator_right");
+            invertAnimation = x < 0;
+        } else {
+            animation = AssetLoader.getInstance().getAnimation("accelerator_up");
+            invertAnimationUp = y > 0;
+        }
+        setInitialSize(animation.getWidth(), animation.getHeight());
     }
 
     @Override
@@ -37,15 +53,30 @@ public class Accelerator extends EntityCollidable {
 
     @Override
     public void render(GameContainer gc, Graphics g) throws SlickException {
+        
+        if (invertAnimationUp) {
+            animation.draw(origin.x - animation.getWidth() / 2,
+                    origin.y + animation.getHeight() / 2,
+                    animation.getWidth(),
+                    -animation.getHeight());
+        } else if (invertAnimation) {
+            animation.draw(origin.x + animation.getWidth() / 2, origin.y
+                    - animation.getHeight() / 2, -animation.getWidth(),
+                    animation.getHeight());
+        } else {
+            animation.draw(origin.x - animation.getWidth() / 2, origin.y
+                    - animation.getHeight() / 2);
+        }
     }
 
     @Override
     public void beginContact(Contact contact) {
         Entity other = getOtherEntity(contact);
-        
-        if (isActive && (other instanceof Astronaut || other instanceof Alien || other instanceof Entity)) {
+
+        if (other instanceof Astronaut || other instanceof Alien || other instanceof Entity) {
+            animation.restart();
             Alien alien = World.getInstance().getAlien();
-            if(other == alien.getCurrentSelected()) {
+            if (other == alien.getCurrentSelected()) {
                 alien.dropCurrentSelected();
             }
             float x = properties.getFloat("velocity_x", 0);
@@ -54,7 +85,7 @@ public class Accelerator extends EntityCollidable {
 //                other.setVelocityY(y);
 //            if(x != 0)
 //                other.setVelocityX(x);
-            
+
             other.getPhysicsObject().applyImpulse(x, y);
         }
     }
