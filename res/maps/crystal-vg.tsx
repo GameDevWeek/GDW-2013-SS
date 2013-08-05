6d086e23838edba82dee7e98f0c1581984f6ac33
albsi27 <albsi27@gmail.com>
2013-08-05 16:38:46 +0000

    Die versprochene Bonus Map mit allen 3 Tilesets, Assets und nem längeren, harten aber machbaren Weg. Und ein paar kleine fixes.

---
+++ res/maps/crystal-vg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="crystal-vg" tilewidth="250" tileheight="250">
+ <image source="crystal-vg.png" width="1000" height="1000"/>
+</tileset>
---
+++ res/maps/swamp-vg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="swamp-vg" tilewidth="250" tileheight="250">
+ <image source="swamp-vg.png" width="1000" height="750"/>
+</tileset>
